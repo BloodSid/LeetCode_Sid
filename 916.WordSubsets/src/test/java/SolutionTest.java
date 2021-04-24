3749480af6255bf53a2d41805a69a3bb89115b08
89228 <892284811@qq.com>
2021-04-24 16:54:05 +0800

    5 tests for problem 916

--- 916.WordSubsets/src/test/java/SolutionTest.java
+++ 916.WordSubsets/src/test/java/SolutionTest.java
@@ -11,17 +11,77 @@ import static org.junit.Assert.*;
  * @create 2021-04-24 16:11
  */
 public class SolutionTest {
-    public static Solution solution=new Solution();
+    public static Solution solution = new Solution();
+
+    private static boolean listEquls(List a, List b) {
+        //TODO 判断两个字符串集合的元素是否相同
+        return false;
+    }
 
     @Test
     public void wordSubsetsTest1() {
-        List<String> list = solution.wordSubsets(new String[]{"amazon","apple","facebook","google","leetcode"},new String[]{"e","o"});
-        List<String> expected=new ArrayList<>();
-        String[] exp = new String[]{"facebook","google","leetcode"};
+        List<String> actual = solution.wordSubsets(
+                new String[]{"amazon", "apple", "facebook", "google", "leetcode"},
+                new String[]{"e", "o"});
+        List<String> expected = new ArrayList<>();
+        String[] exp =
+                new String[]{"facebook", "google", "leetcode"};
         for (int i = 0; i < exp.length; i++) {
             expected.add(exp[i]);
         }
-        assertEquals(true,expected.equals(list));
+        assertEquals(true, listEquls(actual,expected));
+
+    }
+    public void wordSubsetsTest2() {
+        List<String> actual = solution.wordSubsets(
+                new String[]{"amazon", "apple", "facebook", "google", "leetcode"},
+                new String[]{"l","e"});
+        List<String> expected = new ArrayList<>();
+        String[] exp =
+                new String[]{"apple","google","leetcode"};
+        for (int i = 0; i < exp.length; i++) {
+            expected.add(exp[i]);
+        }
+        assertEquals(true, listEquls(actual,expected));
+
+    }
+    public void wordSubsetsTest3() {
+        List<String> actual = solution.wordSubsets(
+                new String[]{"amazon", "apple", "facebook", "google", "leetcode"},
+                new String[]{"e","oo"});
+        List<String> expected = new ArrayList<>();
+        String[] exp =
+                new String[]{"facebook","google"};
+        for (int i = 0; i < exp.length; i++) {
+            expected.add(exp[i]);
+        }
+        assertEquals(true, listEquls(actual,expected));
+
+    }
+    public void wordSubsetsTest4() {
+        List<String> actual = solution.wordSubsets(
+                new String[]{"amazon", "apple", "facebook", "google", "leetcode"},
+                new String[]{"lo","eo"});
+        List<String> expected = new ArrayList<>();
+        String[] exp =
+                new String[]{"google","leetcode"};
+        for (int i = 0; i < exp.length; i++) {
+            expected.add(exp[i]);
+        }
+        assertEquals(true, listEquls(actual,expected));
+
+    }
+    public void wordSubsetsTest5() {
+        List<String> actual = solution.wordSubsets(
+                new String[]{"amazon", "apple", "facebook", "google", "leetcode"},
+                new String[]{"ec","oc","ceo"});
+        List<String> expected = new ArrayList<>();
+        String[] exp =
+                new String[]{"ec","oc","ceo"};
+        for (int i = 0; i < exp.length; i++) {
+            expected.add(exp[i]);
+        }
+        assertEquals(true, listEquls(actual,expected));
 
     }
 
