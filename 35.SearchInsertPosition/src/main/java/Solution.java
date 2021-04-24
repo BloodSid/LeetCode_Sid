9ecc0ded326a7ec388f61e7e1b1465f3f9785c33
89228 <892284811@qq.com>
2021-04-24 14:58:23 +0800

    first solution

---
+++ 35.SearchInsertPosition/src/main/java/Solution.java
@@ -0,0 +1,16 @@
+/**
+ * @author IronSid
+ * @create 2021-04-24 14:50
+ */
+public class Solution {
+    public int searchInsert(int[] nums, int target){
+        for (int index = 0; index < nums.length-1; index++) {
+            if(nums[index]>target){
+                index++;
+            }else if(nums[index]==target){
+                return index;
+            }else return index--;
+        }
+        return 0;
+    }
+}
