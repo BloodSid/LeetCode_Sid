7e776cfe4726244495c79c15fcba272100062684
89228 <892284811@qq.com>
2021-04-26 23:20:52 +0800

    make TreeNode no longer inner class

--- BinaryTree/src/main/java/BinaryTree.java
+++ BinaryTree/src/main/java/BinaryTree.java
@@ -5,24 +5,6 @@
 public class BinaryTree {
 
     //Definition for a binary tree node.
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
 
     /**
      * This method generates a new binary tree with a string.
@@ -34,3 +16,4 @@ public class BinaryTree {
         return null;
     }
 }
+
