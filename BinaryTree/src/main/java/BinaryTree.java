7f1ef60d3d9e96dc238c11f970b178a4e536a8c4
89228 <892284811@qq.com>
2021-04-26 23:13:00 +0800

    create class BinaryTree, prepare for the test of problems about tree

---
+++ BinaryTree/src/main/java/BinaryTree.java
@@ -0,0 +1,36 @@
+/**
+ * @author IronSid
+ * @create 2021-04-26 22:51
+ */
+public class BinaryTree {
+
+    //Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * This method generates a new binary tree with a string.
+     * @param levelTraversal level-order Traversal of the binary tree. Example: "1,null,0,0,1"
+     * @return the root node of the binary tree
+     */
+    public TreeNode stringToBinaryTree(String levelTraversal){
+        //TODO code this method
+        return null;
+    }
+}
