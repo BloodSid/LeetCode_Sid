26d089dd5b7da91c20a7b5a2090cd25dcce21df9
89228 <892284811@qq.com>
2021-04-26 11:49:30 +0800

    refactor solution and all test passed

--- 916.WordSubsets/src/main/java/Solution.java
+++ 916.WordSubsets/src/main/java/Solution.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,46 +9,55 @@ import java.util.List;
 
 public class Solution {
     public List<String> wordSubsets(String[] A, String[] B) {
-        if (A == null||B == null) return new ArrayList<String>();
-        if(A.length==0) return new ArrayList<String>();
-        if(B.length==0) return Arrays.asList(A);
-    //TODO 将b合并成一个单词再去遍历a
-        List<String> uniWords = new ArrayList<>(Arrays.asList(A));
-        //用每一个b去遍历检查A中的所有a
-        for (int i = 0; i < B.length; i++) {
-            if(uniWords.isEmpty()) return uniWords;
-            int indexOfA = 0;
-            while(!uniWords.isEmpty()&&indexOfA<uniWords.size()){
-                //检查B[i]是不是aword的子集
-                String aWord = uniWords.get(indexOfA);
-                char[] charOfaWord = aWord.toCharArray();
-                for (int j = 0; j < B[i].length(); j++) {
-                    boolean isb_jINa = false;
-                    for (int k = 0; k < charOfaWord.length; k++) {
-                        if (charOfaWord[k] == B[i].charAt(j)) {
-                            charOfaWord[k] = '0';
-                            isb_jINa = true;
-                            break;
-                        }
-                    }
-                    if(!isb_jINa){
-                        uniWords.remove(aWord);
-                        break;
-                    }else if (j == B[i].length() - 1) {
-                        indexOfA++;
-                    }
+        if (A == null || B == null) return new ArrayList<String>();
+        if (A.length == 0) return new ArrayList<String>();
+        if (B.length == 0) return Arrays.asList(A);
+        //将b合并成一个单词b_union再去遍历a
+        int[] bmaxChar = new int[26];
+        for (String b :
+                B) {
+            int[] bChar = new int[26];
+            for (int i = 0; i < b.length(); i++) {
+                bChar[b.charAt(i) - 97]++;
+            }
+            for (int i = 0; i < 26; i++) {
+                bmaxChar[i] = Math.max(bmaxChar[i], bChar[i]);
+            }
+        }
+        for (int i = 0; i < bmaxChar.length; i++) {
+            System.out.println((char) ('a' + i)+" "+bmaxChar[i]);
+        }
+        //用a遍历A，检查b——union是不是a的子集，若是则将a加入结果集
+        List<String> uniWords = new ArrayList<>();
+        for (String a :
+                A) {
+            int[] aChar = new int[26];
+            for (int i = 0; i < a.length(); i++) {
+                aChar[a.charAt(i) - 97]++;
+            }
+            System.out.println(a);
+            for (int i = 0; i < aChar.length; i++) {
+                System.out.println((char) ('a' + i)+" "+aChar[i]);
+            }
+            //若a符合条件，则必有a[i]>=bmax[i]
+            for (int i = 0; i < 26; i++) {
+                if (aChar[i] < bmaxChar[i]) {
+                    System.out.println((char) ('a' + i)+"achari "+aChar[i]+"maxchari"+bmaxChar[i]);
+                    System.out.println("delete " + a);
+                    break;
+                } else if (i == 25) {
+                    System.out.println("add " + a);
+                    uniWords.add(a);
                 }
             }
         }
+
+
         return uniWords;
     }
 
     public static void main(String[] args) {
-        String a = new String("Str");
-        String b = new String("Str");
-        System.out.println(a==b);
-        System.out.println(a.equals(b));
-
+        System.out.println((char)('a' + 1));
     }
 
 }
