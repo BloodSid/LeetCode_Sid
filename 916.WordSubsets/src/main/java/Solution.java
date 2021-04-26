bf5bd73ab269dfcdd7ae0c6753797b71df99bf65
89228 <892284811@qq.com>
2021-04-26 11:52:23 +0800

    delele couts

--- 916.WordSubsets/src/main/java/Solution.java
+++ 916.WordSubsets/src/main/java/Solution.java
@@ -9,8 +9,8 @@ import java.util.List;
 
 public class Solution {
     public List<String> wordSubsets(String[] A, String[] B) {
-        if (A == null || B == null) return new ArrayList<String>();
-        if (A.length == 0) return new ArrayList<String>();
+        if (A == null || B == null) return new ArrayList<>();
+        if (A.length == 0) return new ArrayList<>();
         if (B.length == 0) return Arrays.asList(A);
         //将b合并成一个单词b_union再去遍历a
         int[] bmaxChar = new int[26];
@@ -24,9 +24,6 @@ public class Solution {
                 bmaxChar[i] = Math.max(bmaxChar[i], bChar[i]);
             }
         }
-        for (int i = 0; i < bmaxChar.length; i++) {
-            System.out.println((char) ('a' + i)+" "+bmaxChar[i]);
-        }
         //用a遍历A，检查b——union是不是a的子集，若是则将a加入结果集
         List<String> uniWords = new ArrayList<>();
         for (String a :
@@ -35,29 +32,16 @@ public class Solution {
             for (int i = 0; i < a.length(); i++) {
                 aChar[a.charAt(i) - 97]++;
             }
-            System.out.println(a);
-            for (int i = 0; i < aChar.length; i++) {
-                System.out.println((char) ('a' + i)+" "+aChar[i]);
-            }
             //若a符合条件，则必有a[i]>=bmax[i]
             for (int i = 0; i < 26; i++) {
                 if (aChar[i] < bmaxChar[i]) {
-                    System.out.println((char) ('a' + i)+"achari "+aChar[i]+"maxchari"+bmaxChar[i]);
-                    System.out.println("delete " + a);
                     break;
                 } else if (i == 25) {
-                    System.out.println("add " + a);
                     uniWords.add(a);
                 }
             }
         }
-
-
         return uniWords;
-    }
-
-    public static void main(String[] args) {
-        System.out.println((char)('a' + 1));
     }
 
 }
