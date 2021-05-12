9dd62393748290baf4f040e33c141fb1cf150346
89228 <892284811@qq.com>
2021-05-12 23:27:01 +0800

    add question 1162

---
+++ 1162.AsFarFromLandAsPossible/src/main/java/Solution.java
@@ -0,0 +1,11 @@
+/**
+ * @author IronSid
+ * @create 2021-05-12 23:15
+ */
+public class Solution {
+    public int maxDistance(int[][] grid) {
+        return 0;
+
+    }
+
+}
---
+++ 1162.AsFarFromLandAsPossible/src/test/java/SolutionTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author IronSid
+ * @create 2021-05-12 23:19
+ */
+public class SolutionTest {
+
+    @Test
+    public void maxDistanceTest1() {
+        //Input: grid = [[1,0,1],[0,0,0],[1,0,1]]
+        //Output: 2
+        //Explanation: The cell (1, 1) is as far as possible from all the land with distance 2.
+
+    }
+    @Test
+    public void maxDistanceTest2() {
+        //Input: grid = [[1,0,0],[0,0,0],[0,0,0]]
+        //Output: 4
+        //Explanation: The cell (2, 2) is as far as possible from all the land with distance 4.
+
+    }
+}
