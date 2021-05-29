24c6b9eb2ecec7106ea986ae565eeeb6dc7de990
89228 <892284811@qq.com>
2021-05-30 01:03:10 +0800

    逆向思考，用鸡蛋个数和操作次数计算楼层， 当楼层超过n，返回操作次数。通过oj

--- 887.SuperEggDrop/src/main/java/Solution.java
+++ 887.SuperEggDrop/src/main/java/Solution.java
@@ -4,6 +4,7 @@
  */
 public class Solution {
     public int superEggDrop(int k, int n) {
+        //dp[k][t] 表示k个鸡蛋t次操作最多能测试多少层,t最大为n
         int[][] dp = new int[k + 1][n + 1];
         for (int i = 1; i <= n; i++) {
             dp[1][i] = i;
@@ -11,33 +12,17 @@ public class Solution {
         for (int i = 1; i <= k; i++) {
             dp[i][1] = 1;
         }
+
         for (int i = 2; i <= k; i++) {
             for (int j = 2; j <= n; j++) {
-                //dp[i - 1][x - 1] 递增 dp[i][j - x]递减 找出两者相等的点,
-                //即是max(dp[i - 1][x - 1], dp[i][j - x])的最小点
-                int front = 1;
-                int behind = j;
-                int x = (front + behind) / 2;
-                while (front < behind - 1) {
-                    int t1 = dp[i - 1][x - 1];
-                    int t2 = dp[i][j - x];
-                    if(t1<t2){
-                        front=x;
-                    }
-                    if(t1>t2){
-                        behind=x;
-                    }
-                    if(t1==t2){
-                        front=behind=x;
-                    }
-                    x = (front + behind) / 2;
-                }
-                dp[i][j] =Math.min(Math.max(dp[i - 1][x - 1], dp[i][j - x]),
-                                    Math.max(dp[i - 1][x], dp[i][j - x - 1])
-                ) + 1;
+                dp[i][j] = dp[i-1][j-1]+1+dp[i][j-1];
+                if(i==k&&dp[i][j]>=n)
+                    return j;
             }
+
         }
-        return dp[k][n];
+
+        return n;
 
     }
 
