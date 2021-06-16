501b0499cb9e89d1d15ef4248cfd4b2eb03d3966
89228 <892284811@qq.com>
2021-06-16 16:04:13 +0800

    HJ44 dfs, Accepted, 时间41ms

--- HJ44_Sudoku/src/main/java/Main.java
+++ HJ44_Sudoku/src/main/java/Main.java
@@ -6,9 +6,6 @@ import java.util.*;
  */
 public class Main {
     static int[][] s;//Sokudo
-    static List<Integer>[][] l;
-    static Queue<Integer> queue;
-    static List<Integer> lastNums;
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -19,42 +16,10 @@ public class Main {
                     s[i][j] = in.nextInt();
                 }
             }
-            //初始化队列矩阵
-            Integer[] array1to9 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-            l = new ArrayList[9][9];
-            queue = new LinkedList<>();
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    List<Integer> integerList = new ArrayList<>();
-                    if (s[i][j] == 0) {
-                        Collections.addAll(integerList, array1to9);
-                    } else {
-                        integerList.add(s[i][j]);
-                        queue.offer(i * 9 + j);
-                    }
-                    l[i][j] = integerList;
-                }
-            }
-            //通过排除法进行填数
-            while (!queue.isEmpty()) {
-                Integer block = queue.poll();
-                int x = block / 9;
-                int y = block % 9;
-                writeNum(x, y, l[x][y].get(0));
-            }
-            //找出排除法无法确定的位置
-            lastNums=new ArrayList<>();
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    if (s[i][j] == 0) {
-                        lastNums.add(i * 9 + j);
-                    }
-                }
-            }
             //深度优先搜索
             dfsWrite(0);
 
-
+            //输出拼接
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < 9; i++) {
                 for (int j = 0; j < 9; j++) {
@@ -69,48 +34,6 @@ public class Main {
         }
     }
 
-    //true 表示不会产生矛盾 false 表示会产生矛盾
-    //推测法写数字
-    static boolean writeNum(int x, int y, int num) {
-        s[x][y] = num;
-        boolean flag = false;
-        for (int i = 0; i < 9; i++) {
-            if (i != y) {
-                flag = l[x][i].remove((Integer) num);
-                if (flag) {
-                    if (l[x][i].size() == 1)
-                        queue.offer(x * 9 + i);
-                    if (l[x][i].size() == 0)
-                        return false;
-                }
-            }
-        }
-        for (int i = 0; i < 9; i++) {
-            if (i != x) {
-                flag = l[i][y].remove((Integer) num);
-                if (flag) {
-                    if (l[i][y].size() == 1)
-                        queue.offer(i * 9 + y);
-                    if (l[i][y].size() == 0)
-                        return false;
-                }
-            }
-        }
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (x % 3 != i || y % 3 != j) {
-                    flag = l[x / 3 * 3 + i][y / 3 * 3 + j].remove((Integer) num);
-                    if (flag) {
-                        if (l[x / 3 * 3 + i][y / 3 * 3 + j].size() == 1)
-                            queue.offer((x / 3 * 3 + i) * 9 + (y / 3 * 3 + j));
-                        if (l[x / 3 * 3 + i][y / 3 * 3 + j].size() == 0)
-                            return false;
-                    }
-                }
-            }
-        }
-        return true;
-    }
 
     //true 表示不会产生矛盾 false 表示会产生矛盾
     static boolean check(int index) {
@@ -140,23 +63,21 @@ public class Main {
     //dfs写数字
     static boolean dfsWrite(int index) {
         //递归终止标记
-        if (index == lastNums.size()) return true;
+        if (index == 81) return true;
+        int x = index / 9, y = index % 9;
 
-        int x = lastNums.get(index) / 9, y = lastNums.get(index) % 9;
         //若该格子已填则跳过
-        //if (s[x][y] != 0) return dfsWrite(index + 1);
+        if (s[x][y] != 0) return dfsWrite(index + 1);
 
         //若未填则遍历该格子的列表
-        List<Integer> list = l[x][y];
-
-        for (Integer num : list) {
+        for (int num = 1; num <= 9; num++) {
             s[x][y] = num;
-            if (check(lastNums.get(index)) && dfsWrite(index + 1))
+            if (check(index) && dfsWrite(index + 1))
                 return true;
             else
                 s[x][y] = 0;
         }
-
+        //遍历之后，没有成功填数则返回false
         return false;
     }
 }
