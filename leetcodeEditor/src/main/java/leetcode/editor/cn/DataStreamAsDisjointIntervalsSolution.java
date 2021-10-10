3bb676f642772ceba7ba005f0d7b13f6bef0cc85
89228 <892284811@qq.com>
2021-10-10 21:08:44 +0800

    352. 将数据流变为多个不相交区间 使用有序映射维护区间 TreeMap AC 涉及TreeMap.floorEntry()和ceilingEntry()的使用 assertArrayEquals()可以对二维数组使用

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/DataStreamAsDisjointIntervalsSolution.java
@@ -0,0 +1,123 @@
+package leetcode.editor.cn;
+//给你一个由非负整数 a1, a2, ..., an 组成的数据流输入，请你将到目前为止看到的数字总结为不相交的区间列表。 
+//
+// 实现 SummaryRanges 类： 
+//
+// 
+// 
+// 
+// SummaryRanges() 使用一个空数据流初始化对象。 
+// void addNum(int val) 向数据流中加入整数 val 。 
+// int[][] getIntervals() 以不相交区间 [starti, endi] 的列表形式返回对数据流中整数的总结。 
+// 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入：
+//["SummaryRanges", "addNum", "getIntervals", "addNum", "getIntervals", "addNum"
+//, "getIntervals", "addNum", "getIntervals", "addNum", "getIntervals"]
+//[[], [1], [], [3], [], [7], [], [2], [], [6], []]
+//输出：
+//[null, null, [[1, 1]], null, [[1, 1], [3, 3]], null, [[1, 1], [3, 3], [7, 7]],
+// null, [[1, 3], [7, 7]], null, [[1, 3], [6, 7]]]
+//
+//解释：
+//SummaryRanges summaryRanges = new SummaryRanges();
+//summaryRanges.addNum(1);      // arr = [1]
+//summaryRanges.getIntervals(); // 返回 [[1, 1]]
+//summaryRanges.addNum(3);      // arr = [1, 3]
+//summaryRanges.getIntervals(); // 返回 [[1, 1], [3, 3]]
+//summaryRanges.addNum(7);      // arr = [1, 3, 7]
+//summaryRanges.getIntervals(); // 返回 [[1, 1], [3, 3], [7, 7]]
+//summaryRanges.addNum(2);      // arr = [1, 2, 3, 7]
+//summaryRanges.getIntervals(); // 返回 [[1, 3], [7, 7]]
+//summaryRanges.addNum(6);      // arr = [1, 2, 3, 6, 7]
+//summaryRanges.getIntervals(); // 返回 [[1, 3], [6, 7]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= val <= 104 
+// 最多调用 addNum 和 getIntervals 方法 3 * 104 次 
+// 
+// 
+// 
+//
+// 
+//
+// 进阶：如果存在大量合并，并且与数据流的大小相比，不相交区间的数量很小，该怎么办? 
+// Related Topics 设计 二分查找 有序集合 
+// 👍 143 👎 0
+
+
+import java.util.*;
+
+/**
+ * 将数据流变为多个不相交区间
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2021-10-10 20:07:54 
+ */
+public class DataStreamAsDisjointIntervalsSolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class SummaryRanges {
+    TreeMap<Integer, Integer> intervals;
+    public SummaryRanges() {
+        intervals = new TreeMap<>();
+    }
+    
+    public void addNum(int val) {
+        Map.Entry<Integer, Integer> interval0 = intervals.floorEntry(val);
+        Map.Entry<Integer, Integer> interval1 = intervals.ceilingEntry(val + 1);
+        Integer l0 = interval0 == null ? null : interval0.getKey();
+        Integer r0 = interval0 == null ? null : interval0.getValue();
+        Integer l1 = interval1 == null ? null : interval1.getKey();
+        Integer r1 = interval1 == null ? null : interval1.getValue();
+        if (interval0 != null && val >= l0 && val <= r0) {
+            return;
+        }
+        if (interval0 != null && interval1 != null && val == r0 + 1 && val == l1 - 1) {
+            intervals.remove(l1);
+            intervals.put(l0, r1);
+            return;
+        }
+        if (interval0 != null && val == r0 + 1) {
+            intervals.put(l0, val);
+            return;
+        }
+        if (interval1 != null && val == l1 - 1) {
+            intervals.remove(l1);
+            intervals.put(val, r1);
+            return;
+        }
+        intervals.put(val, val);
+    }
+    
+    public int[][] getIntervals() {
+        int[][] ret = new int[intervals.size()][2];
+        int index = 0;
+        for (Map.Entry<Integer, Integer> entry : intervals.entrySet()) {
+            ret[index][0] = entry.getKey();
+            ret[index][1] = entry.getValue();
+            index++;
+        }
+        return ret;
+    }
+}
+
+/**
+ * Your SummaryRanges object will be instantiated and called as such:
+ * SummaryRanges obj = new SummaryRanges();
+ * obj.addNum(val);
+ * int[][] param_2 = obj.getIntervals();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/DataStreamAsDisjointIntervalsSolutionTest.java
@@ -0,0 +1,56 @@
+package leetcode.editor.cn;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.*;
+
+/**
+ * DataStreamAsDisjointIntervalsSolution Tester.
+ *
+ * @author IronSid
+ * @since 10/10/2021
+ * @version 1.0
+ */
+public class DataStreamAsDisjointIntervalsSolutionTest {
+    @Before
+    public void before() throws Exception {
+        
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    @Test
+    public void test1() throws Exception {
+        DataStreamAsDisjointIntervalsSolution.SummaryRanges summaryRanges = new DataStreamAsDisjointIntervalsSolution() .new SummaryRanges();
+        summaryRanges.addNum(1);
+        int[][] actual = summaryRanges.getIntervals();
+        int[][] expected = {{1, 1}};
+        // assertArrayEquals 对二维数组也可用
+        assertArrayEquals(expected, actual);
+        summaryRanges.addNum(3);
+        // 返回 [[1, 1], [3, 3]]
+        actual = summaryRanges.getIntervals();
+        expected = new int[][]{{1, 1}, {3, 3}};
+        assertArrayEquals(expected, actual);
+        summaryRanges.addNum(7);
+        // 返回 [[1, 1], [3, 3], [7, 7]]
+        actual = summaryRanges.getIntervals();
+        expected = new int[][]{{1, 1}, {3, 3}, {7, 7}};
+        assertArrayEquals(expected, actual);
+        summaryRanges.addNum(2);
+        // 返回 [[1, 3], [7, 7]]
+        actual = summaryRanges.getIntervals();
+        expected = new int[][]{{1, 3}, {7, 7}};
+        assertArrayEquals(expected, actual);
+        summaryRanges.addNum(6);
+        // 返回 [[1, 3], [6, 7]]
+        actual = summaryRanges.getIntervals();
+        expected = new int[][]{{1, 3}, {6, 7}};
+        assertArrayEquals(expected, actual);
+    }
+
+}
