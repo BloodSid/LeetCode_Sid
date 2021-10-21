68808cf99e51faad7f86090643c5b8f8d9e7edb2
89228 <892284811@qq.com>
2021-10-22 01:46:26 +0800

    229. 求众数 II 摩尔投票法 1ms ac 空间复杂度为 O(1)

--- leetcodeEditor/src/main/java/leetcode/editor/cn/MajorityElementIiSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/MajorityElementIiSolution.java
@@ -13,16 +13,44 @@ public class MajorityElementIiSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int lowerBound = nums.length / 3;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        List<Integer> ans = new ArrayList<>();
+        // 摩尔投票法的核心思想为对拼消耗
+        int element1 = 0;
+        int element2 = 0;
+        int vote1 = 0;
+        int vote2 = 0;
         for (int num : nums) {
-            int cnt = map.getOrDefault(num, 0);
-            cnt++;
-            if (cnt == lowerBound + 1) {
-                ans.add(num);
+            if (vote1 > 0 && num == element1) {
+                vote1++;
+            } else if (vote2 > 0 && num == element2) {
+                vote2++;
+            } else if (vote1 == 0) {
+                element1 = num;
+                vote1++;
+            } else if (vote2 == 0) {
+                element2 = num;
+                vote2++;
+            } else {
+                // 与前面两个数均不同，则抵消这三个数
+                vote1--;
+                vote2--;
             }
-            map.put(num, cnt);
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for (int num : nums) {
+            if (num == element1) {
+                cnt1++;
+            } else if (num == element2){
+                cnt2++;
+            }
+        }
+        if (cnt1 > nums.length / 3) {
+            ans.add(element1);
+        }
+        if (cnt2 > nums.length / 3) {
+            ans.add(element2);
         }
         return ans;
     }
