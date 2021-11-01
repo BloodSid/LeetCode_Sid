9c7a55dfbe60de5545f25aa310340f297878ce31
89228 <892284811@qq.com>
2021-11-01 11:06:25 +0800

    83. 删除排序链表中的重复元素 迭代 AC

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/RemoveDuplicatesFromSortedListSolution.java
@@ -0,0 +1,29 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+/**
+ * 删除排序链表中的重复元素
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2021-11-01 11:02:07 
+ */
+public class RemoveDuplicatesFromSortedListSolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode cur = head;
+        while (cur != null && cur.next != null){
+            if (cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
