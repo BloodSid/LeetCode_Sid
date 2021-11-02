7d9b142b17dfc6c91e386c14258c4847f9873b4e
89228 <892284811@qq.com>
2021-11-02 16:33:43 +0800

    237. 删除链表中的节点 AC

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/DeleteNodeInALinkedListSolution.java
@@ -0,0 +1,22 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+/**
+ * 删除链表中的节点
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2021-11-02 16:32:18 
+ */
+public class DeleteNodeInALinkedListSolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
