4cd8b1d7c6ec3373ae86940ea27f9beab10cb638
89228 <892284811@qq.com>
2021-11-07 12:16:20 +0800

    435. 无重叠区间 贪心 AC

--- leetcodeEditor/src/main/java/leetcode/editor/cn/NonOverlappingIntervalsSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/NonOverlappingIntervalsSolution.java
@@ -7,26 +7,23 @@ import java.util.*;
  *
  * @author IronSid
  * @version 1.0
- * @since 2021-11-07 11:44:47 
+ * @since 2021-11-07 11:44:47
  */
 public class NonOverlappingIntervalsSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[1]));
         int n = intervals.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
-        int max = 1;
+        int[] pre = intervals[0];
+        int ans = 1;
         for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (intervals[j][1] <= intervals[i][0]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
+            if (intervals[i][0] >= pre[1]) {
+                ans++;
+                pre = intervals[i];
             }
-            max = Math.max(max, dp[i]);
         }
-        return n - max;
+        return n - ans;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
