da2fadc7c818768b03cde2f9f66e7d917ef0da1a
89228 <892284811@qq.com>
2021-11-10 18:54:38 +0800

    763. 划分字母区间 贪心 AC

--- leetcodeEditor/src/main/java/leetcode/editor/cn/PartitionLabelsSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/PartitionLabelsSolution.java
@@ -14,31 +14,21 @@ public class PartitionLabelsSolution {
 class Solution {
     public List<Integer> partitionLabels(String s) {
         char[] arr = s.toCharArray();
-        int[][] intervals = new int[26][2];
-        for (int i = 0; i < intervals.length; i++) {
-            intervals[i][0] = Integer.MAX_VALUE;
-        }
+        int[] last = new int[26];
         for (int i = 0; i < arr.length; i++) {
-            int index = arr[i] - 'a';
-            intervals[index][0] = Math.min(intervals[index][0], i);
-            intervals[index][1] = Math.max(intervals[index][1], i);
+            last[arr[i] - 'a'] = i;
         }
-        Arrays.sort(intervals, (o1, o2) -> o1[0] - o2[0]);
-        List<int[]> fusedIntervals = new ArrayList<>();
-        fusedIntervals.add(intervals[0]);
-        for (int i = 1; i < intervals.length && intervals[i][0] < arr.length; i++) {
-            int[] lastInterval = fusedIntervals.get(fusedIntervals.size() - 1);
-            if (intervals[i][0] < lastInterval[1]) {
-                lastInterval[1] = Math.max(lastInterval[1], intervals[i][1]);
-            } else {
-                fusedIntervals.add(intervals[i]);
+        List<Integer> partion = new ArrayList<>();
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < arr.length; i++) {
+            end = Math.max(end, last[arr[i] - 'a']);
+            if (i == end) {
+                partion.add(end - start + 1);
+                start = end + 1;
             }
         }
-        List<Integer> ans = new ArrayList<>();
-        for (int[] fusedInterval : fusedIntervals) {
-            ans.add(fusedInterval[1] - fusedInterval[0] + 1);
-        }
-        return ans;
+        return partion;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
