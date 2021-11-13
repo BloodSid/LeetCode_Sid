b4990cedde9f571a0302a21c39076f05278381a9
89228 <892284811@qq.com>
2021-11-13 12:33:03 +0800

    142. 环形链表 II 快慢指针 AC

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/LinkedListCycleIiSolution.java
@@ -0,0 +1,39 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+/**
+ * 环形链表 II
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2021-11-13 12:27:20
+ */
+public class LinkedListCycleIiSolution {
+    //leetcode submit region begin(Prohibit modification and deletion)
+    public class Solution {
+        public ListNode detectCycle(ListNode head) {
+            if (head == null) return null;
+            if (head.next == null) return null;
+
+            ListNode s = head;
+            ListNode f = head;
+            while (f != null && f.next != null) {
+                s = s.next;
+                f = f.next.next;
+                if (s == f) break;
+            }
+            if (s == f) {
+                f = head;
+                while (s != f) {
+                    s = s.next;
+                    f = f.next;
+                }
+                return f;
+            }
+            return null;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
