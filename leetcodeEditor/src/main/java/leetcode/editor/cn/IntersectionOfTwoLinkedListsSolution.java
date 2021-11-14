603de04250a7554855a3cfafe0eb52fa172619e6
89228 <892284811@qq.com>
2021-11-14 11:01:55 +0800

    160. 相交链表 双指针 AC 两个指针同时遍历两个链表，一旦其中一个到达结尾，则从另一个链表的头节点继续。若两个指针到同一个节点或同时为null则结束

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/IntersectionOfTwoLinkedListsSolution.java
@@ -0,0 +1,30 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+/**
+ * 相交链表
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2021-11-14 10:47:40
+ */
+public class IntersectionOfTwoLinkedListsSolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+        while (p1 != p2) {
+            p1 = p1 == null ? headB : p1.next;
+            p2 = p2 == null ? headA : p2.next;
+        }
+        return p1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
