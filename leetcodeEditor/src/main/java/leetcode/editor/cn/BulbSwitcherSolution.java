643e8ef28e3cbfaeed3b69a50fe0d57d2e4fe380
89228 <892284811@qq.com>
2021-11-15 16:07:54 +0800

    319. 灯泡开关 数论 脑筋急转弯 AC

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/BulbSwitcherSolution.java
@@ -0,0 +1,21 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+/**
+ * 灯泡开关
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2021-11-15 02:14:28 
+ */
+public class BulbSwitcherSolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int bulbSwitch(int n) {
+        return (int)Math.sqrt(0.5 + n);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
