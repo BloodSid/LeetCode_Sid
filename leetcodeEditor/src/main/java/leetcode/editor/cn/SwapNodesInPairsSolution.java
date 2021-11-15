227e8590adfdb78e8e76c8134a97db78cd6bc22b
89228 <892284811@qq.com>
2021-11-15 14:46:22 +0800

    24. 两两交换链表中的节点 递归 AC

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/SwapNodesInPairsSolution.java
@@ -0,0 +1,26 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+/**
+ * 两两交换链表中的节点
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2021-11-15 02:15:14
+ */
+public class SwapNodesInPairsSolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode newHead = head.next;
+        head.next = swapPairs(newHead.next);
+        newHead.next = head;
+        return newHead;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
