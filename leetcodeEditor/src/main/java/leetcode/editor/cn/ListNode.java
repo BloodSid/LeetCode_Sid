30290dc19553e76657c94bbffd0fdbbd0aeeb9b0
89228 <892284811@qq.com>
2021-11-16 13:31:22 +0800

    ListNode类添加参数为字符串的构造器和toString 方法

--- leetcodeEditor/src/main/java/leetcode/editor/cn/ListNode.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/ListNode.java
@@ -4,11 +4,36 @@ package leetcode.editor.cn;
  * @author IronSid
  * @since 2021-08-29 19:29
  */
- public class ListNode {
-     int val;
-     ListNode next;
-     ListNode() {}
-     ListNode(int val) { this.val = val; }
-     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- }
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    ListNode(String list) {
+        String[] strs = list.split(",");
+        this.val = Integer.parseInt(strs[0]);
+        ListNode curr = this;
+        for (int i = 1; i < strs.length; i++) {
+            curr.next = new ListNode(Integer.parseInt(strs[i]));
+            curr = curr.next;
+        }
+    }
+
+    @Override
+
+    public String toString() {
+        return val + "," + next;
+    }
+}
 
