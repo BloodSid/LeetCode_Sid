8e28e65ff9c9f427cccf088869f17ce767a7527c
89228 <892284811@qq.com>
2021-11-24 13:39:19 +0800

    423. 从英文中重建数字 解方程组  AC 这题本质是解一个非齐次线性方程组，而方程组比较简单可以直接看出解来。更一般的情况应该先用单词表构建系数矩阵，再解线性方程组

--- leetcodeEditor/src/main/java/leetcode/editor/cn/ReconstructOriginalDigitsFromEnglishSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/ReconstructOriginalDigitsFromEnglishSolution.java
@@ -12,22 +12,22 @@ import java.util.*;
 public class ReconstructOriginalDigitsFromEnglishSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    int[][] freqs;
-
     public String originalDigits(String s) {
-        String[] digits = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        freqs = new int[10][26];
-        for (int i = 0; i < digits.length; i++) {
-            for (int j = 0; j < digits[i].length(); j++) {
-                freqs[i][digits[i].charAt(j) - 'a']++;
-            }
-        }
-        int[] sFreq = new int[26];
+        int[] freq = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            sFreq[s.charAt(i) - 'a']++;
+            freq[s.charAt(i) - 'a']++;
         }
         int[] cnt = new int[10];
-        dfs(0, cnt, sFreq);
+        cnt[0] = freq['z' - 'a'];
+        cnt[2] = freq['w' - 'a'];
+        cnt[4] = freq['u' - 'a'];
+        cnt[6] = freq['x' - 'a'];
+        cnt[8] = freq['g' - 'a'];
+        cnt[3] = freq['h' - 'a'] - cnt[8];
+        cnt[5] = freq['f' - 'a'] - cnt[4];
+        cnt[7] = freq['s' - 'a'] - cnt[6];
+        cnt[1] = freq['o' - 'a'] - cnt[0] - cnt[2] - cnt[4];
+        cnt[9] = freq['i' - 'a'] - cnt[5] - cnt[6] - cnt[8];
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i <= 9; i++) {
             for (int i1 = 0; i1 < cnt[i]; i1++) {
@@ -35,43 +35,6 @@ class Solution {
             }
         }
         return sb.toString();
-    }
-
-    boolean dfs(int digit, int[] cnt, int[] sFreq) {
-        if (digit == 10) {
-            return false;
-        }
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < 26; i++) {
-            if (freqs[digit][i] != 0) {
-                min = Math.min(min, sFreq[i] / freqs[digit][i]);
-            }
-        }
-        int[] tem = sFreq.clone();
-        for (int i = 0; i <= min; i++) {
-            cnt[digit] = i;
-            for (int j = 0; j < 26; j++) {
-                sFreq[j] = tem[j] - i * freqs[digit][j];
-            }
-            // 若所有字母剩余数量都是0，则返回true
-            int k = 0;
-            for (; k < 26; k++) {
-                if (sFreq[k] != 0) {
-                    break;
-                }
-            }
-            if (k == 26) {
-                return true;
-            }
-            // 递归运算剩下的数字
-            if (dfs(digit + 1, cnt, sFreq)) {
-                return true;
-            }
-        }
-        // 回溯
-        cnt[digit] = 0;
-        System.arraycopy(tem, 0, sFreq, 0, sFreq.length);
-        return false;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
