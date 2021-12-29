4bfb2409c060334df127442b62264dc8563d29e8
89228 <892284811@qq.com>
2021-12-29 21:20:16 +0800

    剑指 Offer 35. 复杂链表的复制 迭代地创建新节点 使用哈希表储存 新节点和旧节点的对应关系 AC

--- leetcodeEditor/src/main/java/leetcode/editor/cn/FuZaLianBiaoDeFuZhiLcofSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/FuZaLianBiaoDeFuZhiLcofSolution.java
@@ -12,36 +12,20 @@ import java.util.*;
 public class FuZaLianBiaoDeFuZhiLcofSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    Map<Node, Node> cachedNode = new HashMap<>();
+
     public Node copyRandomList(Node head) {
         if (head == null) {
             return null;
         }
-        Node cur = head;
-        Map<Node, Integer> map = new HashMap<>();
-        int index = 0;
-        while (cur != null) {
-            map.put(cur, index);
-            index++;
-            cur = cur.next;
+        // 判断该节点是否已经创建副本。若不判断这一步，可能会导致死循环。
+        if (!cachedNode.containsKey(head)) {
+            Node newHead = new Node(head.val);
+            cachedNode.put(head, newHead);
+            newHead.next = copyRandomList(head.next);
+            newHead.random = copyRandomList(head.random);
         }
-        int n = map.size();
-        int[] vals = new int[n];
-        int[] randoms = new int[n];
-        cur = head;
-        for (int i = 0; i < n; i++) {
-            vals[i] = cur.val;
-            randoms[i] = cur.random == null ? -1 : map.get(cur.random);
-            cur = cur.next;
-        }
-        Node[] arr = new Node[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = new Node(vals[i]);
-        }
-        for (int i = 0; i < n; i++) {
-            arr[i].next = i == n - 1 ? null : arr[i + 1];
-            arr[i].random = randoms[i] == -1 ? null : arr[randoms[i]];
-        }
-        return arr[0];
+        return cachedNode.get(head);
     }
 }
 
