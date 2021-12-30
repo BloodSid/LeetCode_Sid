264f5f6d8e89a8d8cb30c561c37021022be63faf
89228 <892284811@qq.com>
2021-12-30 20:50:04 +0800

    剑指 Offer 03. 数组中重复的数字 把数组通过交换元素排成i == num[i]的形式，排查到不能交换的元素即为重复的元素。算法原地运行，不需要新开一个数组储存每个元素的频率 AC 0ms

--- leetcodeEditor/src/main/java/leetcode/editor/cn/ShuZuZhongZhongFuDeShuZiLcofSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/ShuZuZhongZhongFuDeShuZiLcofSolution.java
@@ -13,14 +13,24 @@ public class ShuZuZhongZhongFuDeShuZiLcofSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int findRepeatNumber(int[] nums) {
-        int[] freq = new int[nums.length];
-        for (int num : nums) {
-            freq[num]++;
-            if (freq[num] > 1) {
-                return num;
+        for (int i = 0; i < nums.length; i++) {
+            // 把nums[i]放到下标为 num[i]的位置上
+            while (nums[i] != i) {
+                if (nums[i] == nums[nums[i]]) {
+                    return nums[i];
+                }
+                swap(nums, i, nums[i]);
             }
+            // 此时从nums[0]到nums[i]的值分别为0 到 i; 数组后面的部分也有一些数字是已经排好的
         }
+        // 全部排好，无重复数字则返回-1
         return -1;
+    }
+
+    void swap(int[] nums, int a, int b) {
+        int t = nums[a];
+        nums[a] = nums[b];
+        nums[b] = t;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
