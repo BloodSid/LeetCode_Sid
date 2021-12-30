5f513636a65c1a584c57103f31d95c8556b68a35
89228 <892284811@qq.com>
2021-12-30 21:57:57 +0800

    剑指 Offer 53 - I. 在排序数组中查找数字 I 使用两次二分搜索 AC

--- leetcodeEditor/src/main/java/leetcode/editor/cn/ZaiPaiXuShuZuZhongChaZhaoShuZiLcofSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/ZaiPaiXuShuZuZhongChaZhaoShuZiLcofSolution.java
@@ -13,27 +13,25 @@ public class ZaiPaiXuShuZuZhongChaZhaoShuZiLcofSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int search(int[] nums, int target) {
-        if (nums.length == 0) return 0;
-        int low = 0, high = nums.length - 1;
-        int m = 0;
-        while (low <= high) {
-            m = (low + high) >>> 1;
-            if (nums[m] == target) {
-                break;
-            } else if (nums[m] > target) {
-                high = m - 1;
-            } else {
-                low = m + 1;
-            }
-        }
-        if (nums[m] != target) {
+        int start = binarySearch(nums, 0, nums.length - 1, target);
+        if (start == nums.length) {
             return 0;
         }
+        int end = binarySearch(nums, start, nums.length - 1, target + 1);
+        return end - start;
+    }
 
-        int start = m, end = m;
-        while (start > 0 && nums[start - 1] == target) start--;
-        while (end < nums.length - 1 && nums[end + 1] == target) end++;
-        return end - start + 1;
+    // 找到一个数或者比他大的数第一次出现的位置
+    int binarySearch(int[] nums, int low, int high, int key) {
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            if (nums[mid] < key) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
