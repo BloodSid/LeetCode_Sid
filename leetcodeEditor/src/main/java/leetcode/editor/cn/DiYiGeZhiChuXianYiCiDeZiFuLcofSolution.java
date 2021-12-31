4f97fb11838ee7f742cc4a39fb8a6013a8bbf27d
89228 <892284811@qq.com>
2021-12-31 22:50:30 +0800

    剑指 Offer 50. 第一个只出现一次的字符 比较每个字母第一次出现和最后一次出现的位置 1ms AC

--- leetcodeEditor/src/main/java/leetcode/editor/cn/DiYiGeZhiChuXianYiCiDeZiFuLcofSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/DiYiGeZhiChuXianYiCiDeZiFuLcofSolution.java
@@ -13,16 +13,18 @@ public class DiYiGeZhiChuXianYiCiDeZiFuLcofSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public char firstUniqChar(String s) {
-        int[] freq = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - 'a']++;
-        }
-        for (int i = 0; i < s.length(); i++) {
-            if (freq[s.charAt(i) - 'a'] == 1) {
-                return s.charAt(i);
+        char ans = ' ';
+        int index = Integer.MAX_VALUE;
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            int head = s.indexOf(ch);
+            if (head != -1 && head == s.lastIndexOf(ch)) {
+                if (head < index) {
+                    index = head;
+                    ans = ch;
+                }
             }
         }
-        return ' ';
+        return ans;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
