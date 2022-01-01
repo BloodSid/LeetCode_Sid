91e7f1bdf584ab9c15ebe956373f2df1566f2590
89228 <892284811@qq.com>
2022-01-01 20:04:40 +0800

    2022. 将一维数组转变成二维数组 这题和566.重塑矩阵类似 AC

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/Convert1dArrayInto2dArraySolution.java
@@ -0,0 +1,27 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+/**
+ * 将一维数组转变成二维数组
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2022-01-01 19:58:48
+ */
+public class Convert1dArrayInto2dArraySolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        if (original.length != m * n) {
+            return new int[0][];
+        }
+        int[][] result = new int[m][n];
+        for (int i = 0; i < m * n; i++) {
+            result[i / n][i % n] = original[i];
+        }
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+}
