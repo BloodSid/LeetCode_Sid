574ca9f72a049fd68df2c14a82918fb2e768dc9f
89228 <892284811@qq.com>
2022-01-01 21:10:26 +0800

    剑指 Offer 32 - II. 从上到下打印二叉树 II 层序遍历，每层在遍历前读出列表的长度，既是层的长度 AC

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/CongShangDaoXiaDaYinErChaShuIiLcofSolution.java
@@ -0,0 +1,42 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+/**
+ * 从上到下打印二叉树 II
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2022-01-01 20:54:38
+ */
+public class CongShangDaoXiaDaYinErChaShuIiLcofSolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        List<List<Integer>> result = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            List<Integer> line = new ArrayList<>();
+            result.add(line);
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+                line.add(cur.val);
+            }
+        }
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
