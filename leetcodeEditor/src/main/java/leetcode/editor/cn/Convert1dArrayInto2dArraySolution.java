0fca23535c0f91495d9009f7d48f490276d8b5cf
89228 <892284811@qq.com>
2022-01-01 20:07:11 +0800

    2022. 将一维数组转变成二维数组 做法二，用乘法代替除法，从4ms优化到3ms AC

--- leetcodeEditor/src/main/java/leetcode/editor/cn/Convert1dArrayInto2dArraySolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/Convert1dArrayInto2dArraySolution.java
@@ -17,8 +17,10 @@ class Solution {
             return new int[0][];
         }
         int[][] result = new int[m][n];
-        for (int i = 0; i < m * n; i++) {
-            result[i / n][i % n] = original[i];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[i][j] = original[n * i + j];
+            }
         }
         return result;
     }
