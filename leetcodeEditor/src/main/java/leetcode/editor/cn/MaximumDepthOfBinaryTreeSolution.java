3dc7bab0c64c6bc2afd5c66435bc837dba843ba0
89228 <892284811@qq.com>
2022-01-06 21:32:24 +0800

    104. 二叉树的最大深度 递归 AC

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/MaximumDepthOfBinaryTreeSolution.java
@@ -0,0 +1,24 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+/**
+ * 二叉树的最大深度
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2022-01-06 21:28:38
+ */
+public class MaximumDepthOfBinaryTreeSolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
