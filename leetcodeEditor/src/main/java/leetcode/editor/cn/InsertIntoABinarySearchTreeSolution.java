31dba6c6f9f018bb6e12fbe1d3d495d66b33dd6b
89228 <892284811@qq.com>
2022-01-08 23:59:18 +0800

    701. 二叉搜索树中的插入操作 递归 AC

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/InsertIntoABinarySearchTreeSolution.java
@@ -0,0 +1,29 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+/**
+ * 二叉搜索树中的插入操作
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2022-01-08 23:50:35
+ */
+public class InsertIntoABinarySearchTreeSolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
