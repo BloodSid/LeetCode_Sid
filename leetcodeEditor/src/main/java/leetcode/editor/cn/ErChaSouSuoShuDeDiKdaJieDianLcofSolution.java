f7865984f2536f03df99631cdcc5e4605d943a2b
89228 <892284811@qq.com>
2022-01-10 11:24:01 +0800

    剑指 Offer 54. 二叉搜索树的第k大节点 和《230. 二叉搜索树中第K小的元素》类似 中序遍历 迭代 栈 AC

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/ErChaSouSuoShuDeDiKdaJieDianLcofSolution.java
@@ -0,0 +1,34 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+/**
+ * 二叉搜索树的第k大节点
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2022-01-10 09:42:58
+ */
+public class ErChaSouSuoShuDeDiKdaJieDianLcofSolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int kthLargest(TreeNode root, int k) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.right;
+            }
+            root = stack.pop();
+            k--;
+            if (k == 0) {
+                return root.val;
+            }
+            root = root.left;
+        }
+        return 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
