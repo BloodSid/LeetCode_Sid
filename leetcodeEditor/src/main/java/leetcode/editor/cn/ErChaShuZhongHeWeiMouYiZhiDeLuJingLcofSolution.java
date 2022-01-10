e39f20669a1b6c726d1e87c7a2911793ae895624
89228 <892284811@qq.com>
2022-01-10 11:15:24 +0800

    剑指 Offer 34. 二叉树中和为某一值的路径 与《113. 路径总和 II》相同 DFS AC

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/ErChaShuZhongHeWeiMouYiZhiDeLuJingLcofSolution.java
@@ -0,0 +1,47 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+/**
+ * 二叉树中和为某一值的路径
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2022-01-10 09:42:48 
+ */
+public class ErChaShuZhongHeWeiMouYiZhiDeLuJingLcofSolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    List<List<Integer>> ans;
+    Deque<Integer> stack;
+    int targetSum;
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        ans = new ArrayList<>();
+        stack = new LinkedList<>();
+        if (root == null) {
+            return ans;
+        }
+        this.targetSum = targetSum;
+        dfs(root, 0);
+        return ans;
+    }
+
+    void dfs(TreeNode root, int sum) {
+        sum += root.val;
+        stack.addLast(root.val);
+        if (root.left != null) {
+            dfs(root.left, sum);
+        }
+        if (root.right != null) {
+            dfs(root.right, sum);
+        }
+        if (root.left == null && root.right == null && sum == targetSum) {
+            ans.add(new ArrayList<>(stack));
+        }
+        stack.removeLast();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
