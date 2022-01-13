a40e9da28c086cdab756b36e82da3e62e2ad3474
89228 <892284811@qq.com>
2022-01-13 14:14:17 +0800

    剑指 Offer 55 - I. 二叉树的深度 和 《104. 二叉树的最大深度》一样 AC

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/ErChaShuDeShenDuLcofSolution.java
@@ -0,0 +1,24 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+/**
+ * 二叉树的深度
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2022-01-13 13:33:50
+ */
+public class ErChaShuDeShenDuLcofSolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
