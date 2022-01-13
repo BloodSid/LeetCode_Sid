81c16b74daaa57d387001f8d64a5607f947591b3
89228 <892284811@qq.com>
2022-01-13 15:22:40 +0800

    剑指 Offer 55 - II. 平衡二叉树 自底向上的递归 如果一棵子树是平衡的，则返回其高度（高度一定是非负整数），否则返回-1。AC 0ms

--- leetcodeEditor/src/main/java/leetcode/editor/cn/PingHengErChaShuLcofSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/PingHengErChaShuLcofSolution.java
@@ -13,18 +13,24 @@ public class PingHengErChaShuLcofSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-        return isBalanced(root.left) && isBalanced(root.right)
-                && Math.abs(getDepth(root.left) - getDepth(root.right)) <= 1;
+        return getDepth(root) >= 0;
     }
 
+    // 若不平衡，返回-1；若平衡返回深度
     int getDepth(TreeNode node) {
         if (node == null) {
             return 0;
         }
-        return Math.max(getDepth(node.left), getDepth(node.right)) + 1;
+        int leftDepth = getDepth(node.left);
+        int rightDepth = getDepth(node.right);
+        // 若节点的子树不平衡，则该节点不平衡
+        if (leftDepth == -1 || rightDepth == -1) {
+            return -1;
+        }
+        if (Math.abs(leftDepth - rightDepth) > 1) {
+            return -1;
+        }
+        return Math.max(leftDepth, rightDepth) + 1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
