69b530d3805820b6895cbdd7018aebca523310f2
89228 <892284811@qq.com>
2022-01-14 15:50:35 +0800

    剑指 Offer 68 - I. 二叉搜索树的最近公共祖先 迭代 AC 和《235. 二叉搜索树的最近公共祖先》相同

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/ErChaSouSuoShuDeZuiJinGongGongZuXianLcofSolution.java
@@ -0,0 +1,32 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+/**
+ * 二叉搜索树的最近公共祖先
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2022-01-14 12:52:22
+ */
+public class ErChaSouSuoShuDeZuiJinGongGongZuXianLcofSolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int v1 = Math.min(p.val, q.val);
+        int v2 = p.val == v1 ? q.val : p.val;
+        while (true) {
+            if (root.val > v2) {
+                root = root.left;
+            } else if (root.val < v1) {
+                root = root.right;
+            } else {
+                break;
+            }
+        }
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
