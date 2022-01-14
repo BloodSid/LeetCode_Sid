db1fffbae7e78809f81cccb19a5489e377d30cbc
89228 <892284811@qq.com>
2022-01-14 15:44:23 +0800

    剑指 Offer 68 - II. 二叉树的最近公共祖先 递归 和《236. 二叉树的最近公共祖先》相同 AC

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/ErChaShuDeZuiJinGongGongZuXianLcofSolution.java
@@ -0,0 +1,35 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+/**
+ * 二叉树的最近公共祖先
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2022-01-14 12:52:23
+ */
+public class ErChaShuDeZuiJinGongGongZuXianLcofSolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left != null) {
+            return left;
+        }
+        return right;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
