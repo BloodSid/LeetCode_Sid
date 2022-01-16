4a7167db83ea02629329269a7e894295004e6838
89228 <892284811@qq.com>
2022-01-16 10:19:26 +0800

    剑指 Offer 65. 不用加减乘除做加法 二进制加法 迭代 AC

--- leetcodeEditor/src/main/java/leetcode/editor/cn/BuYongJiaJianChengChuZuoJiaFaLcofSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/BuYongJiaJianChengChuZuoJiaFaLcofSolution.java
@@ -7,20 +7,23 @@ import java.util.*;
  *
  * @author IronSid
  * @version 1.0
- * @since 2022-01-16 09:50:32 
+ * @since 2022-01-16 09:50:32
  */
 public class BuYongJiaJianChengChuZuoJiaFaLcofSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    // int用补码存储，所以对正数负数的加法通用
     public int add(int a, int b) {
+        int carry;
         // 执行到进位为0时结束
-        if (b == 0) {
-            return a;
+        while (b != 0) {
+            // 进位
+            carry = (a & b) << 1;
+            // 无进位和
+            a ^= b;
+            b = carry;
         }
-        int xorSum = a ^ b;
-        // 这里写 int carry = (~xorSum) & a; 也可以
-        int carry = a & b;
-        return add(xorSum, (carry << 1));
+        return a;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
