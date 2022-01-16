47910299d0f9946d0af944715c53e3231211b609
89228 <892284811@qq.com>
2022-01-16 12:01:14 +0800

    剑指 Offer 33. 二叉搜索树的后序遍历序列 单调栈 迭代 AC 1ms 用例比较小，所以用O(n^2)的方法才能达到0ms

--- leetcodeEditor/src/main/java/leetcode/editor/cn/ErChaSouSuoShuDeHouXuBianLiXuLieLcofSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/ErChaSouSuoShuDeHouXuBianLiXuLieLcofSolution.java
@@ -12,37 +12,23 @@ import java.util.*;
 public class ErChaSouSuoShuDeHouXuBianLiXuLieLcofSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    int[] postorder;
-    int[] lastSmaller;
-
     public boolean verifyPostorder(int[] postorder) {
-        this.postorder = postorder;
-        lastSmaller = new int[postorder.length];
-        // 单调栈，确定数组中每个元素的上一个更小元素的下标
-        Deque<Integer> stack = new LinkedList<>();
-        for (int i = 0; i < postorder.length; i++) {
-            while (!stack.isEmpty() && postorder[stack.peek()] >= postorder[i]) {
-                stack.pop();
+        Deque<Integer> stack = new ArrayDeque<>();
+        int root = Integer.MAX_VALUE;
+        // 逆序遍历 [根，右，左]
+        for (int i = postorder.length - 1; i >= 0; i--) {
+            // 左子树必小于根节点
+            if (postorder[i] > root) {
+                return false;
             }
-            lastSmaller[i] = stack.isEmpty() ? -1 : stack.peek();
-            stack.push(i);
+            // 一但当前节点小于栈顶节点，说明当前节点是某个节点的左儿子，就要通过循环出栈找到这个节点
+            while (!stack.isEmpty() && stack.peek() > postorder[i]) {
+                // 最早入栈且大于当前节点的节点是根节点，当前节点是它的左儿子。
+                root = stack.pop();
+            }
+            stack.push(postorder[i]);
         }
-        return verifyPostorder(0, postorder.length - 1, Integer.MIN_VALUE, Integer.MAX_VALUE);
-    }
-
-    // left 和 right 表示一个子树在数组中对应的范围
-    boolean verifyPostorder(int left, int right, int lower, int upper) {
-        // 空节点
-        if (left > right) {
-            return true;
-        }
-        // postorder[right]是该子树的根节点
-        if (postorder[right] <= lower || postorder[right] >= upper) {
-            return false;
-        }
-        // 递归检查左右子树
-        return verifyPostorder(left, lastSmaller[right], lower, postorder[right])
-                && verifyPostorder(lastSmaller[right] + 1, right - 1, postorder[right], upper);
+        return true;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
