6abf36a8290ce6ecdb69f09b2e93df9fd971d8bd
89228 <892284811@qq.com>
2022-01-18 13:35:48 +0800

    剑指 Offer 62. 圆圈中最后剩下的数字 去掉无关注释

--- leetcodeEditor/src/main/java/leetcode/editor/cn/YuanQuanZhongZuiHouShengXiaDeShuZiLcofSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/YuanQuanZhongZuiHouShengXiaDeShuZiLcofSolution.java
@@ -16,7 +16,6 @@ class Solution {
         // f(1, k) = 0
         int f = 0;
         // f(n, k) = (f(n - 1, k) + k) % n
-        // 防止刚好整除的情形导致取模为0，对原值-1后再对模+1
         for (int i = 2; i <= n; i++) {
             f = (f + m) % i;
         }
