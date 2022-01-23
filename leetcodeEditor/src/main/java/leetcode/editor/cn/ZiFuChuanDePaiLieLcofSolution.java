85744cf70602a1fe6d79c6096347507051210263
89228 <892284811@qq.com>
2022-01-23 13:54:56 +0800

    剑指 Offer 38. 字符串的排列 回溯 哈希表 AC 19ms

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/ZiFuChuanDePaiLieLcofSolution.java
@@ -0,0 +1,42 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+/**
+ * 字符串的排列
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2022-01-23 13:43:14
+ */
+public class ZiFuChuanDePaiLieLcofSolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    Set<String> set;
+
+    public String[] permutation(String s) {
+        set = new HashSet<>();
+        char[] arr = s.toCharArray();
+        dfs(arr, 0);
+        return set.toArray(new String[0]);
+    }
+
+    void dfs(char[] arr, int index) {
+        if (index == arr.length) {
+            set.add(new String(arr));
+            return;
+        }
+        char temp = arr[index];
+        for (int i = index; i < arr.length; i++) {
+            // if (i != index)
+            arr[index] = arr[i];
+            arr[i] = temp;
+            dfs(arr, index + 1);
+            arr[i] = arr[index];
+        }
+        arr[index] = temp;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
