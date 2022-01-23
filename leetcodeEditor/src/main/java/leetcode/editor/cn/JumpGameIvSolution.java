11796ddc0dd8b3e83bd4430c214208e009c6d3fa
89228 <892284811@qq.com>
2022-01-23 17:00:35 +0800

    1345. 跳跃游戏 IV BFS 去掉多余的visited, 只用indexMap就可以避免重复入队 AC 40ms

--- leetcodeEditor/src/main/java/leetcode/editor/cn/JumpGameIvSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/JumpGameIvSolution.java
@@ -14,7 +14,6 @@ public class JumpGameIvSolution {
 class Solution {
     public int minJumps(int[] arr) {
         HashMap<Integer, List<Integer>> indexMap = new HashMap<>();
-        HashSet<Integer> visited = new HashSet<>();
         for (int i = 0; i < arr.length; i++) {
             // 若是连续连续相同数字则跳过
             if (i > 0 && arr[i - 1] == arr[i] && i < arr.length - 1 && arr[i + 1] == arr[i]) {
@@ -28,7 +27,6 @@ class Solution {
         int step = 0;
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(0);
-        visited.add(0);
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
@@ -37,13 +35,11 @@ class Solution {
                     return step;
                 }
                 // 相邻的点都入队
-                // indexMap 和 visited共同记录是否访问过
-                if (curr > 0 && arr[curr] != arr[curr - 1] && indexMap.containsKey(arr[curr - 1])
-                        && visited.add(curr - 1)) {
+                // indexMap记录是否访问过
+                if (curr > 0 && arr[curr] != arr[curr - 1] && indexMap.containsKey(arr[curr - 1])) {
                     queue.offer(curr - 1);
                 }
-                if (arr[curr] != arr[curr + 1] && indexMap.containsKey(arr[curr + 1])
-                        && visited.add(curr + 1)) {
+                if (arr[curr] != arr[curr + 1] && indexMap.containsKey(arr[curr + 1])) {
                     queue.offer(curr + 1);
                 }
                 if (indexMap.containsKey(arr[curr])) {
