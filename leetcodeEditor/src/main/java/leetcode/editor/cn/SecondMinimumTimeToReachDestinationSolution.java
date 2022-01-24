178be5823a0d1894cd938bebaaa6cac287833bf7
89228 <892284811@qq.com>
2022-01-24 15:44:06 +0800

    2045. 到达目的地的第二短时间  BFS 每个点都保留两个最早且不重复的到达时间，并依此剪枝 201ms AC

--- leetcodeEditor/src/main/java/leetcode/editor/cn/SecondMinimumTimeToReachDestinationSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/SecondMinimumTimeToReachDestinationSolution.java
@@ -25,31 +25,37 @@ class Solution {
         }
         // 堆里的元素 {p, t, arrival} p表示点，t表示已经经过的时间，arrival表示从别的点到达该点
         int ARRIVAL = 1, WAIT = 0;
-        int fast = 0;
         PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
         pq.offer(new int[]{1, 0, WAIT});
+        // 每个点都保留两个最早且不重复的到达时间，并依此剪枝
+        Map<Integer, int[]> arrivalTable = new HashMap<>();
         while (!pq.isEmpty()) {
-            // pq.forEach(o -> System.out.print(Arrays.toString(o)));
-            // System.out.println();
             int[] cur = pq.poll();
             int p = cur[0];
             int t = cur[1];
-            if (p == n && cur[2] == ARRIVAL) {
-                if (fast == 0) {
-                    fast = t;
-                } else if (t == fast) {
-                    continue;
-                } else {
-                    return t;
-                }
+            if (cur[2] == ARRIVAL && p == n && arrivalTable.getOrDefault(n, new int[2])[1] != 0) {
+                return arrivalTable.get(n)[1];
             }
             if ((t / change & 1) == 1) {
                 // 红灯
-                pq.offer(new int[]{p, (t / change + 1) * change, WAIT});
+                int nt = (t / change + 1) * change;
+                pq.offer(new int[]{p, nt, WAIT});
             } else {
                 // 绿灯
+                int nt = t + time;
                 for (Integer neighbor : map.get(p)) {
-                    pq.offer(new int[]{neighbor, t + time, ARRIVAL});
+                    arrivalTable.putIfAbsent(neighbor, new int[2]);
+                    int[] arrivalTimes = arrivalTable.get(neighbor);
+                    if (arrivalTimes[0] == 0) {
+                        arrivalTimes[0] = nt;
+                    } else if (nt == arrivalTimes[0]) {
+                        continue;
+                    } else if (arrivalTimes[1] == 0) {
+                        arrivalTimes[1] = nt;
+                    } else {
+                        continue;
+                    }
+                    pq.offer(new int[]{neighbor, nt, ARRIVAL});
                 }
             }
         }
