e2cc5f58ad9b05694316a3cf44f9ef04d47b9495
89228 <892284811@qq.com>
2022-01-25 20:51:57 +0800

    1201. 丑数 III 二分搜索 最小公倍数 最大公约数 AC 0ms

--- leetcodeEditor/src/main/java/leetcode/editor/cn/UglyNumberIiiSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/UglyNumberIiiSolution.java
@@ -12,26 +12,47 @@ import java.util.*;
 public class UglyNumberIiiSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    long a, b, c, abLCM, bcLCM, acLCM, abcLCM;
+
     public int nthUglyNumber(int n, int a, int b, int c) {
-        long[][] multi = {{a, 1}, {b, 1}, {c, 1}};
-        PriorityQueue<long[]> pq = new PriorityQueue<>(Comparator.comparingLong(o -> o[0] * o[1]));
-        for (long[] longs : multi) {
-            pq.offer(longs);
-        }
-        long ugly = 0;
-        for (int i = 0; i < n; i++) {
-            long[] tem = pq.poll();
-            ugly = tem[0] * tem[1];
-            tem[1]++;
-            pq.offer(tem);
-            // 堆中最多有三个相等的丑数，此时这个数是3个因数的公因数
-            for (int i1 = 0; i1 < 2 && ugly == pq.peek()[0] * pq.peek()[1]; i1++) {
-                tem = pq.poll();
-                tem[1]++;
-                pq.offer(tem);
+        int l = 1, r = (int) 2e9;
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        abLCM = lcm(a, b);
+        bcLCM = lcm(b, c);
+        acLCM = lcm(a, c);
+        abcLCM = lcm(abLCM, c);
+        while (l < r) {
+            int mid = l + (r - l >>> 1);
+            if (getCnt(mid) < n) {
+                l = mid + 1;
+            } else {
+                r = mid;
             }
         }
-        return (int) ugly;
+        return l;
+    }
+
+    // 计算小于等于n的丑数的个数
+    int getCnt(int n) {
+        return (int) (n / a + n / b + n / c - n / abLCM - n / acLCM - n / bcLCM + n / abcLCM);
+    }
+
+    // 辗转相除法 gcd(a,b)=gcd(b,a mod b) greatest common divisor
+    long gcd(long a, long b) {
+        long c = 0;
+        while (b != 0) {
+            c = b;
+            b = a % b;
+            a = c;
+        }
+        return a;
+    }
+
+    // 最小公倍数 least common multiple
+    long lcm(long a, long b) {
+        return a * b / gcd(a, b);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
