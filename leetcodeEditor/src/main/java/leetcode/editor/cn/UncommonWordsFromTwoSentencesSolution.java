d5bf656af321ee8b30dcefa144af10847010c152
89228 <892284811@qq.com>
2022-01-30 00:16:05 +0800

    884. 两句话中的不常见单词 哈希表 简化逻辑，原条件等价于找到在两个句子中一共只出现一次的单词 AC 5ms 耗时应该主要在split方法

--- leetcodeEditor/src/main/java/leetcode/editor/cn/UncommonWordsFromTwoSentencesSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/UncommonWordsFromTwoSentencesSolution.java
@@ -13,24 +13,18 @@ public class UncommonWordsFromTwoSentencesSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public String[] uncommonFromSentences(String s1, String s2) {
-        HashMap<String, Integer> map1 = new HashMap<>();
-        HashMap<String, Integer> map2 = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
         for (String s : s1.split("\\s")) {
-            map1.put(s, map1.getOrDefault(s, 0) + 1);
+            map.put(s, map.getOrDefault(s, 0) + 1);
         }
         for (String s : s2.split("\\s")) {
-            map2.put(s, map2.getOrDefault(s, 0) + 1);
+            map.put(s, map.getOrDefault(s, 0) + 1);
         }
         List<String> res = new ArrayList<>();
-        for (int i = 0; i < 2; i++) {
-            for (String s : map1.keySet()) {
-                if (map1.get(s) == 1 && !map2.containsKey(s)) {
-                    res.add(s);
-                }
+        for (String s : map.keySet()) {
+            if (map.get(s) == 1) {
+                res.add(s);
             }
-            HashMap<String, Integer> temp = map1;
-            map1 = map2;
-            map2 = temp;
         }
         return res.toArray(new String[0]);
     }
