faf51f21ad5afabd355618d92500dce35136e02c
89228 <892284811@qq.com>
2022-02-17 11:31:51 +0800

    688. 骑士在棋盘上的概率 动态规划 AC 4ms

--- leetcodeEditor/src/main/java/leetcode/editor/cn/KnightProbabilityInChessboardSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/KnightProbabilityInChessboardSolution.java
@@ -1,7 +1,5 @@
 package leetcode.editor.cn;
 
-import java.util.*;
-
 /**
  * 骑士在棋盘上的概率
  *
@@ -12,34 +10,37 @@ import java.util.*;
 public class KnightProbabilityInChessboardSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    int n;
     int[][] dirs = {{1, 2}, {2, 1}, {-1, 2}, {-2, 1}, {1, -2}, {2, -1}, {-1, -2}, {-2, -1}};
-    HashMap<Integer, Double> keyToP = new HashMap<>();
 
     public double knightProbability(int n, int k, int row, int column) {
-        this.n = n;
-        return p(k, row, column);
-    }
-
-    double p(int k, int row, int col) {
-        if (row < 0 || row >= n || col < 0 || col >= n) {
-            return 0;
-        }
-        if (k == 0) {
-            return 1;
-        }
-        int key = (k << 16) + (row << 8) + col;
-        if (keyToP.containsKey(key)) {
-            return keyToP.get(key);
+        double[][] curr = new double[n][n];
+        double[][] next = new double[n][n];
+        curr[row][column] = 1;
+        for (int i1 = 0; i1 < k; i1++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    double sum = 0;
+                    for (int[] dir : dirs) {
+                        int nRow = i + dir[0];
+                        int nCol = j + dir[1];
+                        if (nRow < 0 || nRow >= n || nCol < 0 || nCol >= n) {
+                            continue;
+                        }
+                        sum += curr[nRow][nCol];
+                    }
+                    next[i][j] = sum / 8.0;
+                }
+            }
+            double[][] temp = curr;
+            curr = next;
+            next = temp;
         }
         double sum = 0;
-        for (int[] dir : dirs) {
-            int nRow = row + dir[0];
-            int nCol = col + dir[1];
-            sum += p(k - 1, nRow, nCol);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sum += curr[i][j];
+            }
         }
-        sum /= 8.0;
-        keyToP.put(key, sum);
         return sum;
     }
 }
