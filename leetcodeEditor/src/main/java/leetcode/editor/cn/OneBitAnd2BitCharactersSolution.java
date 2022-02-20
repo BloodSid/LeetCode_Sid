cce0df9943159d95af96c8776522af0c54f15357
89228 <892284811@qq.com>
2022-02-20 22:20:13 +0800

    717. 1比特与2比特字符 简单模拟 正序遍历 AC 0ms

--- leetcodeEditor/src/main/java/leetcode/editor/cn/OneBitAnd2BitCharactersSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/OneBitAnd2BitCharactersSolution.java
@@ -14,20 +14,11 @@ public class OneBitAnd2BitCharactersSolution {
 class Solution {
     public boolean isOneBitCharacter(int[] bits) {
         int n = bits.length;
-        boolean[] dp = new boolean[n + 1];
-        dp[0] = true;
-        for (int i = 1; i <= n; i++) {
-            if (bits[i - 1] == 0) {
-                dp[i] |= dp[i - 1];
-            }
-            if (i >= 2 && bits[i - 2] == 1) {
-                dp[i] |= dp[i - 2];
-            }
+        int idx = 0;
+        while (idx < n - 1) {
+            idx += bits[idx] == 1 ? 2 : 1;
         }
-        if (n >= 2 && dp[n - 2] && bits[n - 2] == 1) {
-            return false;
-        }
-        return dp[n];
+        return idx == n - 1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
