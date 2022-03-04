767ffe481973b6b490b96b8c6f435d7fe8224c41
89228 <892284811@qq.com>
2022-03-04 20:09:21 +0800

    2104. 子数组范围和 单调栈 优化，只要从前向后遍历一次，就可以得到左右两侧最近的比当前数小或大的数 AC 13ms 时间复杂度是O(N)

--- leetcodeEditor/src/main/java/leetcode/editor/cn/SumOfSubarrayRangesSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/SumOfSubarrayRangesSolution.java
@@ -24,34 +24,24 @@ class Solution {
         // 栈底最大
         Deque<Integer> maxStack = new LinkedList<>();
         // 若数组中两个元素相等，比如nums[i] == nums[j] 则这两个元素的大小由i和j的大小决定
-        // 所以从前向后和从后向前分别遍历数组的时候，比较当前元素和栈顶元素的操作符是不同的
         for (int i = 0; i < n; i++) {
             int cur = nums[i];
             while (!minStack.isEmpty() && nums[minStack.peek()] > cur) {
-                minStack.pop();
+                minRight[minStack.pop()] = i;
             }
             minLeft[i] = minStack.isEmpty() ? -1 : minStack.peek();
             minStack.push(i);
             while (!maxStack.isEmpty() && nums[maxStack.peek()] <= cur) {
-                maxStack.pop();
+                maxRight[maxStack.pop()] = i;
             }
             maxLeft[i] = maxStack.isEmpty() ? -1 : maxStack.peek();
             maxStack.push(i);
         }
-        minStack.clear();
-        maxStack.clear();
-        for (int i = n - 1; i >= 0; i--) {
-            int cur = nums[i];
-            while (!minStack.isEmpty() && nums[minStack.peek()] >= cur) {
-                minStack.pop();
-            }
-            minRight[i] = minStack.isEmpty() ? n : minStack.peek();
-            minStack.push(i);
-            while (!maxStack.isEmpty() && nums[maxStack.peek()] < cur) {
-                maxStack.pop();
-            }
-            maxRight[i] = maxStack.isEmpty() ? n : maxStack.peek();
-            maxStack.push(i);
+        while (!minStack.isEmpty()) {
+            minRight[minStack.pop()] = n;
+        }
+        while (!maxStack.isEmpty()) {
+            maxRight[maxStack.pop()] = n;
         }
         // res = sumMax - sumMin, 下面的循环相当于于分别求出这两个和并相减
         for (int i = 0; i < n; i++) {
