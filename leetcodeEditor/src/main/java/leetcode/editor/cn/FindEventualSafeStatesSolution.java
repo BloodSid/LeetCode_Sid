ffbf6a383f8bad3810068247f1d5b3025633ebd0
89228 <892284811@qq.com>
2022-03-10 16:10:54 +0800

    802. 找到最终的安全状态 dfs + 三色标记法 AC 4ms

--- leetcodeEditor/src/main/java/leetcode/editor/cn/FindEventualSafeStatesSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/FindEventualSafeStatesSolution.java
@@ -12,55 +12,33 @@ import java.util.*;
 public class FindEventualSafeStatesSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    // 一个点出发的边的链表的头节点 he，边指向的点 e，边在链表中的下一个边ne
-    int[] he, e, ne;
-    int idx = 0;
-    void add(int a, int b) {
-        e[idx] = b;
-        ne[idx] = he[a];
-        he[a] = idx;
-        idx++;
-    }
+    // colors[i] 可以是0，1，2。0表示未访问，1表示在递归栈中或者在环中，2表示已访问且是安全节点
+    int[] colors;
+    int[][] graph;
 
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int n = graph.length;
-        // 边的数量
-        int m = n * 4;
-        // 初始化
-        he = new int[n];
-        e = new int[m];
-        ne = new int[m];
-        idx = 0;
-        Arrays.fill(he, -1);
-        // 反向存图，并统计入度
-        int[] inCnt = new int[n];
-        for (int i = 0; i < graph.length; i++) {
-            for (int j : graph[i]) {
-                add(j, i);
-                inCnt[i]++;
-            }
-        }
-        // 使用队列进行拓扑排序
-        Deque<Integer> queue = new ArrayDeque<>();
-        // 把入度为零的节点（对应原图中出度为零的节点即安全节点）入队
-        for (int i = 0; i < n; i++) {
-            if (inCnt[i] == 0) queue.offer(i);
-        }
-        while (!queue.isEmpty()) {
-            int cur = queue.poll();
-            // 遍历当前点的出边，把这些边所进入的点的入度减一
-            for (int edge = he[cur]; edge != -1; edge = ne[edge]) {
-                int to = e[edge];
-                inCnt[to]--;
-                if (inCnt[to] == 0) queue.offer(to);
-            }
-        }
-        // 拓扑排序之后入度为零的就是安全节点
+        colors = new int[n];
+        this.graph = graph;
         List<Integer> safe = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            if (inCnt[i] == 0) safe.add(i);
+            if (dfs(i)) {
+                safe.add(i);
+            }
         }
         return safe;
+    }
+
+    boolean dfs(int node) {
+        if (colors[node] != 0) {
+            return colors[node] == 2;
+        }
+        colors[node] = 1;
+        for (int next : graph[node]) {
+            if(!dfs(next)) return false;
+        }
+        colors[node] = 2;
+        return true;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
