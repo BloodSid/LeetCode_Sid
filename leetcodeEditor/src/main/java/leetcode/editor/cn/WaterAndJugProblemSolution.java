0110d3ad5aabaa5c6a7c028a02c237a9a285a76d
89228 <892284811@qq.com>
2022-03-14 17:00:40 +0800

    365. 水壶问题 数学 裴蜀定理 AC 0ms

--- leetcodeEditor/src/main/java/leetcode/editor/cn/WaterAndJugProblemSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/WaterAndJugProblemSolution.java
@@ -1,7 +1,5 @@
 package leetcode.editor.cn;
 
-import java.util.*;
-
 /**
  * 水壶问题
  *
@@ -12,41 +10,22 @@ import java.util.*;
 public class WaterAndJugProblemSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public static final long N = (int) (1e6) + 1;
-
     public boolean canMeasureWater(int jug1Capacity, int jug2Capacity, int targetCapacity) {
-        HashSet<Long> visited = new HashSet<>();
-        // 队列中的元素{i,j}, i 表示罐1, j 表示罐2
-        Deque<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[]{0, 0});
-        visited.add(0 * N + 0);
-        while (!queue.isEmpty()) {
-            int[] poll = queue.poll();
-            int i = poll[0], j = poll[1];
-            if (j == targetCapacity || i == targetCapacity || i + j == targetCapacity) {
-                return true;
-            }
-            // 装满任意一个水壶，清空任意一个水壶，从一个壶向另一个壶倒水
-            int[][] next = {{jug1Capacity, j}, {i, jug2Capacity}, {0, j}, {i, 0}, {i + j, 0}, {0, i + j}};
-            // 倒水的时候倒满了
-            if (next[4][0] > jug1Capacity) {
-                next[4][1] = next[5][0] - jug1Capacity;
-                next[4][0] = jug1Capacity;
-            }
-            if (next[5][1] > jug2Capacity) {
-                next[5][0] = next[5][1] - jug2Capacity;
-                next[5][1] = jug2Capacity;
-            }
-            for (int[] ints : next) {
-                int ni = ints[0], nj = ints[1];
-                long v = ni * N + nj;
-                if (!visited.contains(v)) {
-                    visited.add(v);
-                    queue.offer(ints);
-                }
-            }
+        // 设两个罐子的容积分别是 a, b, 目标容积是 m,
+        // 排除等价状态后，每次操作只会让罐子里的总水量增加a，增加b，减少a，减少b
+        // 那么当　ax + by = m　有解时，可以倒出目标体积
+        // ax + by = m 若有解，则 m 是 gcd(a, b) 的倍数
+        return targetCapacity <= jug1Capacity + jug2Capacity && targetCapacity % gcd(jug1Capacity, jug2Capacity) == 0;
+    }
+
+    int gcd(int a, int b) {
+        // gcd(a,b) = gcd(b, a mod b)
+        while (b != 0) {
+            int t = a % b;
+            a = b;
+            b = t;
         }
-        return false;
+        return a;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
