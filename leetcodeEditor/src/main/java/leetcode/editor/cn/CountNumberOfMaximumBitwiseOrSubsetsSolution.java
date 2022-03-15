dfee6d15b7693915b389ef0d3372d447a8e8ae1e
89228 <892284811@qq.com>
2022-03-15 16:03:53 +0800

    2044. 统计按位或能得到最大值的子集数目 位运算 枚举 48ms AC

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/CountNumberOfMaximumBitwiseOrSubsetsSolution.java
@@ -0,0 +1,38 @@
+package leetcode.editor.cn;
+
+/**
+ * 统计按位或能得到最大值的子集数目
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2022-03-15 00:15:27 
+ */
+public class CountNumberOfMaximumBitwiseOrSubsetsSolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int countMaxOrSubsets(int[] nums) {
+        int max = 0;
+        for (int num : nums) {
+            max |= num;
+        }
+        int n = nums.length;
+        int cnt = 0;
+        loop:
+        for (int i = 0; i < 1 << n; i++) {
+            int or = 0;
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) != 0) {
+                    or |= nums[j];
+                }
+                if (or == max) {
+                    cnt++;
+                    continue loop;
+                }
+            }
+        }
+        return cnt;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
