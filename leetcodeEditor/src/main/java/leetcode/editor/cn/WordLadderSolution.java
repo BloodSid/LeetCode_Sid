4f258a46e9b56020d3bd4d5df284f9c5130f5320
89228 <892284811@qq.com>
2022-04-05 00:06:58 +0800

    127. 单词接龙 BFS 虚拟节点优化建图 AC 30ms

--- leetcodeEditor/src/main/java/leetcode/editor/cn/WordLadderSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/WordLadderSolution.java
@@ -12,30 +12,53 @@ import java.util.*;
 public class WordLadderSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+
+    private int n;
+
+    void add(HashMap<String, List<String>> map, String from, String to) {
+        if (!map.containsKey(from)) {
+            map.put(from, new ArrayList<>());
+        }
+        map.get(from).add(to);
+    }
+
+    void addEdges(String word, HashMap<String, List<String>> map) {
+        char[] array = word.toCharArray();
+        for (int i = 0; i < n; i++) {
+            char ch = array[i];
+            array[i] = '*';
+            String temp = new String(array);
+            add(map, word, temp);
+            add(map, temp, word);
+            array[i] = ch;
+        }
+    }
+
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        int n = beginWord.length();
-        HashSet<String> set = new HashSet<>(wordList);
-        int depth = 1;
+        n = beginWord.length();
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String s : wordList) {
+            addEdges(s, map);
+        }
+        addEdges(beginWord, map);
+        int depth = 0;
         Deque<String> queue = new ArrayDeque<>();
+        HashSet<String> visited = new HashSet<>();
         queue.offer(beginWord);
+        visited.add(beginWord);
         while (!queue.isEmpty()) {
             int size = queue.size();
-            for (int j = 0; j < size; j++) {
+            for (int i = 0; i < size; i++) {
                 String s = queue.poll();
                 if (s.equals(endWord)) {
-                    return depth;
+                    return depth / 2 + 1;
                 }
-                for (int i = 0; i < n; i++) {
-                    char cur = s.charAt(i);
-                    for (char c = 'a'; c <= 'z'; c++) {
-                        if (c != cur) {
-                            String next = s.substring(0, i) + c + s.substring(i + 1);
-                            // 若删除成功，则说明该节点未被遍历，加入队中
-                            if (set.remove(next)) {
-                                queue.offer(next);
-                            }
-                        }
+                for (String child : map.get(s)) {
+                    if (visited.contains(child)) {
+                        continue;
                     }
+                    queue.offer(child);
+                    visited.add(child);
                 }
             }
             depth++;
