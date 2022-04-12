d3b48295c7480de9de0223a4c088d8da2e37e2b0
89228 <892284811@qq.com>
2022-04-13 01:03:31 +0800

    380. O(1) 时间插入、删除和获取随机元素 变长数组和哈希表 AC 19ms 执行删除的时候，把变长数组中的元素交换到结尾，再删除，保证O(1)复杂度，getRandom的操作时间复杂度是O(1)

--- leetcodeEditor/src/main/java/leetcode/editor/cn/InsertDeleteGetrandomO1Solution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/InsertDeleteGetrandomO1Solution.java
@@ -7,33 +7,50 @@ import java.util.*;
  *
  * @author IronSid
  * @version 1.0
- * @since 2022-04-13 00:26:49 
+ * @since 2022-04-13 00:26:49
  */
 public class InsertDeleteGetrandomO1Solution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class RandomizedSet {
 
-    HashSet<Integer> set = new HashSet<>();
+    HashMap<Integer, Integer> map = new HashMap<>();
+    ArrayList<Integer> list = new ArrayList<>();
     Random random = new Random();
 
     public RandomizedSet() {
+    }
 
-    }
-    
     public boolean insert(int val) {
-        return set.add(val);
-    }
-    
-    public boolean remove(int val) {
-        return set.remove(val);
-    }
-    
-    public int getRandom() {
-        int size = set.size();
-        for (Integer i : set) {
-            if (random.nextInt(size--) == 0) return i;
+        if (!map.containsKey(val)) {
+            int idx = list.size();
+            list.add(val);
+            map.put(val, idx);
+            return true;
         }
-        return 0;
+        return false;
+    }
+
+    public boolean remove(int val) {
+        if (map.containsKey(val)) {
+            int idx = map.remove(val);
+            int last = list.size() - 1;
+            if (idx == last) {
+                list.remove(last);
+            } else {
+                // 把list中被删除元素替换成list结尾的元素，再将list结尾删除一个元素
+                int lastVal = list.get(last);
+                list.set(idx, lastVal);
+                list.remove(last);
+                // 修改结尾元素在map中对应的下标
+                map.put(lastVal, idx);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public int getRandom() {
+        return list.get(random.nextInt(list.size()));
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
