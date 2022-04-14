9d04d2780516731b6c3ba16ac0feb5a9b2fcc097
89228 <892284811@qq.com>
2022-04-15 00:03:53 +0800

    729. 我的日程安排表 I TreeMap 23ms AC

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/MyCalendarISolution.java
@@ -0,0 +1,32 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+/**
+ * 我的日程安排表 I
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2022-04-14 00:41:01
+ */
+public class MyCalendarISolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class MyCalendar {
+
+    private final TreeMap<Integer, Integer> map;
+
+    public MyCalendar() {
+        map = new TreeMap<>();
+    }
+
+    public boolean book(int start, int end) {
+        Integer pre = map.floorKey(start), suf = map.ceilingKey(start);
+        if (pre != null && start < map.get(pre)) return false;
+        if (suf != null && end > suf) return false;
+        map.put(start, end);
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
