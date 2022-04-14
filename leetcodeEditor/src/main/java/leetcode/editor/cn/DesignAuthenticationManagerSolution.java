bec9ef1909dddd6b53c056cb5e7dbd38cef5d358
89228 <892284811@qq.com>
2022-04-15 01:23:47 +0800

    1797. 设计一个验证系统 哈希表 + 列表 + 懒更新 AC 26ms 统计未过期验证码时遍历记录列表，验证过期的记录的id是否过期并删除过期的id

--- leetcodeEditor/src/main/java/leetcode/editor/cn/DesignAuthenticationManagerSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/DesignAuthenticationManagerSolution.java
@@ -14,35 +14,49 @@ public class DesignAuthenticationManagerSolution {
 class AuthenticationManager {
 
     private int timeToLive;
-    private final HashMap<String, Integer> map;
+    private final HashMap<String, Integer> map = new HashMap<>();
+    // 使用两个列表分别储存id和time
+    List<String> idList = new ArrayList<>();
+    List<Integer> timeList = new ArrayList<>();
+    // 记录上一次查询未过时验证码时，第一个未过时的验证码的index
+    int start = 0;
 
     public AuthenticationManager(int timeToLive) {
         this.timeToLive = timeToLive;
-        map = new HashMap<>();
     }
-    
+
     public void generate(String tokenId, int currentTime) {
         map.put(tokenId, currentTime);
+        idList.add(tokenId);
+        timeList.add(currentTime);
     }
-    
+
     public void renew(String tokenId, int currentTime) {
         if (map.containsKey(tokenId)) {
+            // 若该id存在且未过时，则更新map并给列表添加记录
             if (map.get(tokenId) > currentTime - timeToLive) {
                 map.put(tokenId, currentTime);
+                idList.add(tokenId);
+                timeList.add(currentTime);
             }
         }
     }
-    
+
     public int countUnexpiredTokens(int currentTime) {
-        List<String> list = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            if (entry.getValue() <= currentTime - timeToLive) {
-                list.add(entry.getKey());
+        int n = idList.size();
+        int i = start;
+        for (; i < n; i++) {
+            String id = idList.get(i);
+            Integer time = timeList.get(i);
+            // 遇到第一个没超时的就停止遍历
+            if (time > currentTime - timeToLive) break;
+            // 如果这一id的最晚更新时间和列表中的时间相等，则说明这一id已经过时
+            if (map.get(id).equals(time)) {
+                map.remove(id);
             }
         }
-        for (String s : list) {
-            map.remove(s);
-        }
+        // 下一次查询从start开始即可
+        start = i;
         return map.size();
     }
 }
