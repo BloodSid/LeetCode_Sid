98c77c3033fb1021d1a454c8a8bd982d8fabec57
89228 <892284811@qq.com>
2022-04-16 01:06:32 +0800

    479. 最大回文数乘积 穷举 175ms AC

--- leetcodeEditor/src/main/java/leetcode/editor/cn/LargestPalindromeProductSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/LargestPalindromeProductSolution.java
@@ -11,9 +11,27 @@ public class LargestPalindromeProductSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int largestPalindrome(int n) {
-        long[] palindromes = {0, 9, 9009, 906609, 99000099, 9966006699L, 999000000999L, 99956644665999L,
-                9999000000009999L};
-        return (int) (palindromes[n] % 1337);
+        // 3 * 3 = 9
+        if (n == 1) return 9;
+        long a = (long) Math.pow(10, n);
+        // 例如 99 * 99 = 9801 所以从97开始穷举
+        for (long i = a - 3; i >= 10; i--) {
+            // 反转 i 作为 p 的后半部分
+            long p = 0;
+            for (long t = i; t > 0; t /= 10) {
+                p *= 10;
+                p += t % 10;
+            }
+            // 加上前半部分
+            p += i * a;
+            // p 除以 i 得 a 余 i 的反转，所以要穷举比 i 更大的数
+            for (long j = i + 1; j < a; j++) {
+                if (p % j == 0) {
+                    return (int) (p % 1337);
+                }
+            }
+        }
+        return -1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
