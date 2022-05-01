9ba3e0e7e1e2cba2e437a75ecc2619549ff15650
89228 <892284811@qq.com>
2022-05-01 12:19:20 +0800

    633. 平方数之和 数学 费马平方和定理：一个非负整数 c 如果能够表示为两个整数的平方和，当且仅当 c 的所有形如 4k+3 的质因子的幂均为偶数。时间复杂度O(sqrt(n)) AC 1ms

--- leetcodeEditor/src/main/java/leetcode/editor/cn/SumOfSquareNumbersSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/SumOfSquareNumbersSolution.java
@@ -5,21 +5,23 @@ package leetcode.editor.cn;
  *
  * @author IronSid
  * @version 1.0
- * @since 2022-04-19 09:32:12 
+ * @since 2022-04-19 09:32:12
  */
 public class SumOfSquareNumbersSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean judgeSquareSum(int c) {
-        for (long i = 0, j = (int) Math.sqrt(c); i <= j; ) {
-            long sum = i * i + j * j;
-            if (sum == c) {
-                return true;
+        for (int factor = 2; factor <= c / factor; factor++) {
+            int cnt = 0;
+            while (c % factor == 0) {
+                c /= factor;
+                cnt++;
             }
-            if (sum > c) j--;
-            else i++;
+            if (cnt % 2 == 1 && factor % 4 == 3) {
+                return false;
+            }
         }
-        return false;
+        return c % 4 != 3;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
