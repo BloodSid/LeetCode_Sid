0650e28d529d5e0cbb683ea5c76c9d8fc3762e2b
89228 <892284811@qq.com>
2022-06-10 01:15:24 +0800

    322. 零钱兑换 动态规划 AC 12ms

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/CoinChangeSolution.java
@@ -0,0 +1,71 @@
+package leetcode.editor.cn;
+//给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。 
+//
+// 计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。 
+//
+// 你可以认为每种硬币的数量是无限的。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：coins = [1, 2, 5], amount = 11
+//输出：3 
+//解释：11 = 5 + 5 + 1 
+//
+// 示例 2： 
+//
+// 
+//输入：coins = [2], amount = 3
+//输出：-1 
+//
+// 示例 3： 
+//
+// 
+//输入：coins = [1], amount = 0
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= coins.length <= 12 
+// 1 <= coins[i] <= 2³¹ - 1 
+// 0 <= amount <= 10⁴ 
+// 
+// 👍 1973 👎 0
+
+
+/**
+ * 零钱兑换
+ *
+ * @author IronSid
+ * @since 2022-06-09 23:53:17 
+ * @version 1.0
+ */
+public class CoinChangeSolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0) return 0;
+        int[] dp = new int[amount + 1];
+        int n = coins.length;
+        for (int i = 0; i < n; i++) {
+            int cur = coins[i];
+            if (cur <= amount) dp[cur] = 1;
+            for (int j = 1; j <= amount - cur; j++) {
+                // 没有组合能组成该金额 j, 则跳过
+                if (dp[j] == 0) continue;
+                dp[j + cur] = dp[j + cur] == 0 ? dp[j] + 1 :Math.min(dp[j + cur], dp[j] + 1);
+            }
+        }
+        if (dp[amount] == 0) return -1;
+        return dp[amount];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
