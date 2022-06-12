33fe89895761e1e9db4d4f6685935463e366e7bf
89228 <892284811@qq.com>
2022-06-13 01:09:18 +0800

    221. 最大正方形 动态规划 简化逻辑 4ms AC

--- leetcodeEditor/src/main/java/leetcode/editor/cn/MaximalSquareSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/MaximalSquareSolution.java
@@ -50,43 +50,25 @@ public class MaximalSquareSolution {
 class Solution {
     public int maximalSquare(char[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
-        // 从上往下的长度
-        int[][] dp0 = new int[m][n];
         // 左上角的正方形的边长
-        int[][] dp2 = new int[m][n];
+        int[][] dp = new int[m][n];
         for (int i = 0; i < n; i++) {
-            dp0[0][i] = matrix[0][i] - '0';
-            dp2[0][i] = matrix[0][i] - '0';
-        }
-        for (int i = 1; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == '0') dp0[i][j] = 0;
-                else dp0[i][j] = dp0[i - 1][j] + 1;
-            }
-        }
-        // 从左往右的长度
-        int[][] dp1 = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            dp1[i][0] = matrix[i][0] - '0';
-            dp2[i][0] = matrix[i][0] - '0';
+            dp[0][i] = matrix[0][i] - '0';
         }
         for (int i = 0; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][j] == '0') dp1[i][j] = 0;
-                else dp1[i][j] = dp1[i][j - 1] + 1;
-            }
+            dp[i][0] = matrix[i][0] - '0';
         }
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == '1') {
-                    dp2[i][j] = Math.min(Math.min(dp0[i - 1][j], dp1[i][j - 1]), dp2[i - 1][j - 1]) + 1;
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
                 }
             }
         }
         int max = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                max = Math.max(max, dp2[i][j]);
+                max = Math.max(max, dp[i][j]);
             }
         }
         return max * max;
