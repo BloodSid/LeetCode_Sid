10ea9b039bad3019cec01683051c98da7cbb0c0b
89228 <892284811@qq.com>
2022-06-25 00:33:08 +0800

    剑指 Offer II 091. 粉刷房子 动态规划 + 滚动数组 AC 0ms

--- leetcodeEditor/src/main/java/leetcode/editor/cn/JEj789Solution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/JEj789Solution.java
@@ -55,16 +55,20 @@ public class JEj789Solution {
 class Solution {
     public int minCost(int[][] costs) {
         int n = costs.length;
-        int[][] dp = new int[n][3];
+        int[] dp = new int[3];
         for (int i = 0; i < 3; i++) {
-            dp[0][i] = costs[0][i];
+            dp[i] = costs[0][i];
         }
+        int[] temp = new int[3];
         for (int i = 1; i < n; i++) {
-            dp[i][0] = costs[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
-            dp[i][1] = costs[i][1] + Math.min(dp[i - 1][2], dp[i - 1][0]);
-            dp[i][2] = costs[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
+            temp[0] = costs[i][0] + Math.min(dp[1], dp[2]);
+            temp[1] = costs[i][1] + Math.min(dp[2], dp[0]);
+            temp[2] = costs[i][2] + Math.min(dp[0], dp[1]);
+            int[] t = dp;
+            dp = temp;
+            temp = t;
         }
-        return Math.min(dp[n - 1][0], Math.min(dp[n - 1][1], dp[n - 1][2]));
+        return Math.min(dp[0], Math.min(dp[1], dp[2]));
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
