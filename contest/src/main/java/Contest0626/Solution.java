6b86575273aa4ad068ba6cf1707328ce06db0743
89228 <892284811@qq.com>
2022-06-26 12:49:23 +0800

    从树中删除边的最小分数 周赛T4 回溯 枚举不同的切割方法 TLE

--- contest/src/main/java/Contest0626/Solution.java
+++ contest/src/main/java/Contest0626/Solution.java
@@ -65,9 +65,16 @@ public class Solution {
     }
 
     // T4
+    private List[] map;
+    private int[] parent;
+    private int[] xorSum;
+    private boolean[] visited;
+    private int[] nums;
+
     public int minimumScore(int[] nums, int[][] edges) {
+        this.nums = nums;
         int n = nums.length;
-        List[] map = new List[n];
+        map = new List[n];
         for (int i = 0; i < n; i++) {
             map[i] = new ArrayList<Integer>();
         }
@@ -76,45 +83,55 @@ public class Solution {
             map[edge[0]].add(edge[1]);
         }
         // 以0为根节点，建树
-        int[] parent = new int[n];
-        int[] xorSum = new int[n];
-        boolean[] visited = new boolean[n];
-        dfs(0, parent, xorSum, map, visited);
+        parent = new int[n];
+        xorSum = new int[n];
+        visited = new boolean[n];
+        dfs(0);
         int min = Integer.MAX_VALUE;
         // 枚举删除两条边
         for (int i = 0; i < edges.length; i++) {
+            int root1 = parent[edges[i][0]] == edges[i][1] ? edges[i][0] : edges[i][1];
             for (int j = i + 1; j < edges.length; j++) {
+                int root2 = parent[edges[j][0]] == edges[j][1] ? edges[j][0] : edges[j][1];
                 int x1 = 0, x2 = 0, x3 = 0;
-
+                // root1 是 root2 的 sub
+                if (isSub(root1, root2)) {
+                    x1 = xorSum[root1]; x2 = xorSum[root2] ^ xorSum[root1]; x3 = xorSum[0] ^ xorSum[root2];
+                } else if (isSub(root2, root1)) {
+                    // root2 是 root1 的 sub
+                    x1 = xorSum[root2]; x2 = xorSum[root1] ^ xorSum[root2]; x3 = xorSum[0] ^ xorSum[root1];
+                } else {
+                    x1 = xorSum[root1]; x2 = xorSum[root2]; x3 = xorSum[0] ^ x1 ^ x2;
+                }
                 min = Math.min(min, getScore(x1, x2, x3));
             }
         }
         return min;
     }
 
-    private void dfs(int node, int[] parent, int[] xorSum, List[] map, boolean[] visited) {
+    private void dfs(int node) {
         visited[node] = true;
-        int xor = 0;
+        int xor = nums[node];
         for (Object o : map[node]) {
             Integer chlid = (Integer) o;
             if (visited[chlid]) continue;
             parent[chlid] = node;
-            dfs(chlid, parent, xorSum, map, visited);
+            dfs(chlid);
             xor ^= xorSum[chlid];
         }
         xorSum[node] = xor;
     }
 
     // node1 是否 node2 的 sub
-    boolean isSub(int node1, int node2, int[] parent) {
+    boolean isSub(int node1, int node2) {
         // 根节点不是任何节点的子树
         if (node1 == 0) return false;
         if (parent[node1] == node2) return true;
-        return isSub(parent[node1], node2, parent);
+        return isSub(parent[node1], node2);
     }
 
     int getScore(int ... v) {
-        int max = v[0], min = v[1];
+        int max = v[0], min = v[0];
         for (int i = 1; i < v.length; i++) {
             max = Math.max(max, v[i]);
             min = Math.min(min, v[i]);
