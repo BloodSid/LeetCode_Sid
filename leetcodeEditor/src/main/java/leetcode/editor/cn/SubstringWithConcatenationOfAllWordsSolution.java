a7239f53600647517c74c88906780933909ab380
89228 <892284811@qq.com>
2022-06-28 23:33:23 +0800

    30. 串联所有单词的子串 哈希表 + 滑动窗口 AC 10ms

--- leetcodeEditor/src/main/java/leetcode/editor/cn/SubstringWithConcatenationOfAllWordsSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/SubstringWithConcatenationOfAllWordsSolution.java
@@ -13,39 +13,53 @@ public class SubstringWithConcatenationOfAllWordsSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        HashSet<String> dict = new HashSet<>();
-        Collections.addAll(dict, words);
         int len = words[0].length();
         int m = words.length;
         char[] ch = s.toCharArray();
         int n = ch.length;
-        HashSet<String> cur = new HashSet<>();
-        // 目标子串的长度 target
+        Map<String, Integer> dict = new HashMap<>();
+        int[] f = new int[m];
+        for (String word : words) {
+            Integer idx = dict.get(word);
+            if (idx == null) {
+                int size = dict.size();
+                dict.put(word, size);
+                idx = size;
+            }
+            f[idx]++;
+        }
+        int[] copy = Arrays.copyOf(f, f.length);
         ArrayList<Integer> res = new ArrayList<>();
-        for (int offset = 0; offset < len; offset++) {
+        for (int offset = 0; offset < len; offset++, f = Arrays.copyOf(copy, f.length)) {
             // 滑动窗口
             int l = offset, r = offset;
             for (; r + len <= n; r += len) {
                 String sub = new String(ch, r, len);
-                if (!dict.contains(sub)) {
+                if (!dict.containsKey(sub)) {
                     l = r + len;
-                    cur.clear();
+                    f = Arrays.copyOf(copy, f.length);
+                    continue;
                 }
-                if (cur.contains(sub)) {
+                int idx = dict.get(sub);
+                if (f[idx] == 0) {
                     while (l < r) {
                         String del = new String(ch, l, len);
                         if (sub.equals(del)) {
                             l += len;
                             break;
                         } else {
-                            cur.remove(del);
+                            f[dict.get(del)]++;
                             l += len;
                         }
                     }
                 } else {
-                    cur.add(sub);
+                    f[idx]--;
                 }
-                if (cur.size() == m) res.add(l);
+                int i;
+                for (i = 0; i < f.length; i++) {
+                    if (f[i] != 0) break;
+                }
+                if (i == f.length) res.add(l);
             }
         }
         return res;
