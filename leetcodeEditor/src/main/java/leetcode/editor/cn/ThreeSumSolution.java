f4e116ff05076bb472be7da8a024f3accae7f042
89228 <892284811@qq.com>
2022-07-05 21:39:02 +0800

    15. 三数之和 排序 + 双指针 优化：实现一个用数组储存结果的匿名内部类，直接返回该类的一个实例 0ms AC

--- leetcodeEditor/src/main/java/leetcode/editor/cn/ThreeSumSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/ThreeSumSolution.java
@@ -7,32 +7,56 @@ import java.util.*;
  *
  * @author IronSid
  * @version 1.0
- * @since 2021-11-05 00:21:12 
+ * @since 2021-11-05 00:21:12
  */
 public class ThreeSumSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(nums);
-        for (int num1 = 0; num1 < nums.length; num1++) {
-            if (num1 > 0 && nums[num1] == nums[num1 - 1]) {
-                continue;
+        return new java.util.AbstractList<List<Integer>>() {
+
+            public static final int N = (int) 5e4;
+            int size = 0;
+
+            // data 提交时应改为 static
+            List[] data = new List[N];
+            boolean initialized = false;
+
+            void init() {
+                if (initialized) return;
+                Arrays.sort(nums);
+                for (int p1 = 0; p1 < nums.length; p1++) {
+                    if (p1 > 0 && nums[p1] == nums[p1 - 1]) {
+                        continue;
+                    }
+                    int p3 = nums.length - 1;
+                    for (int p2 = p1 + 1; p2 < p3; p2++) {
+                        if (p2 > p1 + 1 && nums[p2] == nums[p2 - 1]) {
+                            continue;
+                        }
+                        while (p2 < p3 && nums[p1] + nums[p2] + nums[p3] > 0) {
+                            p3--;
+                        }
+                        if (p2 != p3 && nums[p1] + nums[p2] + nums[p3] == 0) {
+                            data[size++] = Arrays.asList(nums[p1], nums[p2], nums[p3]);
+                        }
+                    }
+                }
+                initialized = true;
             }
-            int num3 = nums.length - 1;
-            for (int num2 = num1 + 1; num2 < num3; num2++) {
-                if (num2 > num1 + 1 && nums[num2] == nums[num2 - 1]) {
-                    continue;
-                }
-                while (num2 < num3 && nums[num1] + nums[num2] + nums[num3] > 0) {
-                    num3--;
-                }
-                if (num2 != num3 && nums[num1] + nums[num2] + nums[num3] == 0) {
-                    ans.add(Arrays.asList(nums[num1], nums[num2], nums[num3]));
-                }
+
+            @Override
+            public List<Integer> get(int index) {
+                init();
+                return data[index];
             }
-        }
-        return ans;
+
+            @Override
+            public int size() {
+                init();
+                return size;
+            }
+        };
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
