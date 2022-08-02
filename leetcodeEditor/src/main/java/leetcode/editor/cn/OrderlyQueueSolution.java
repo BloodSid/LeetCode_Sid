829adc3081314bfa302491ddff5833299b283cc0
89228 <892284811@qq.com>
2022-08-03 01:10:06 +0800

    899. 有序队列 分类 模拟 AC 2ms

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/OrderlyQueueSolution.java
@@ -0,0 +1,79 @@
+package leetcode.editor.cn;
+//给定一个字符串 s 和一个整数 k 。你可以从 s 的前 k 个字母中选择一个，并把它加到字符串的末尾。 
+//
+// 返回 在应用上述步骤的任意数量的移动后，字典上最小的字符串 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "cba", k = 1
+//输出："acb"
+//解释：
+//在第一步中，我们将第一个字符（“c”）移动到最后，获得字符串 “bac”。
+//在第二步中，我们将第一个字符（“b”）移动到最后，获得最终结果 “acb”。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "baaca", k = 3
+//输出："aaabc"
+//解释：
+//在第一步中，我们将第一个字符（“b”）移动到最后，获得字符串 “aacab”。
+//在第二步中，我们将第三个字符（“c”）移动到最后，获得最终结果 “aaabc”。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= k <= S.length <= 1000 
+// s 只由小写字母组成。 
+// 
+// 👍 65 👎 0
+
+
+/**
+ * 有序队列
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2022-08-03 00:55:36 
+ */
+public class OrderlyQueueSolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String orderlyQueue(String s, int k) {
+        if (k == 1) {
+            // 相当于字符串轮转
+            String res = s;
+            for (int i = 1; i < s.length(); i++) {
+                String rotate = s.substring(i) + s.substring(0, i);
+                if (res.compareTo(rotate) > 0) {
+                    res = rotate;
+                }
+            }
+            return res;
+        } else {
+            // 相当于随意排列
+            char[] ch = s.toCharArray();
+            int[] f = new int[128];
+            for (char c : ch) {
+                f[c]++;
+            }
+            int i = 0;
+            for (char c = 'a'; c <= 'z'; c++) {
+                while (f[c]-- > 0) {
+                    ch[i++] = c;
+                }
+            }
+            return new String(ch);
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
