01e8904d752c99d855bf4e57c1d84f1a07ad1d26
89228 <892284811@qq.com>
2022-08-06 18:33:55 +0800

    540. 有序数组中的单一元素 二分搜索+ 异或 AC 0ms

--- leetcodeEditor/src/main/java/leetcode/editor/cn/SingleElementInASortedArraySolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/SingleElementInASortedArraySolution.java
@@ -1,7 +1,5 @@
 package leetcode.editor.cn;
 
-import java.util.*;
-
 /**
  * 有序数组中的单一元素
  *
@@ -13,17 +11,13 @@ public class SingleElementInASortedArraySolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
         int l = 0;
-        int r = nums.length - 1;
-        while (l < r) {
-            int mid = l + r >>> 1;
-            // 只搜索偶数下标
-            mid -= mid & 1;
-            if (nums[mid] == nums[mid + 1]) {
-                l = mid + 2;
-            } else {
-                r = mid;
-            }
+        int r = n - 1;
+        while (l <= r) {
+            int mid = l + r >> 1;
+            if (mid != n - 1 && nums[mid] == nums[mid ^ 1]) l = mid + 1;
+            else r = mid - 1;
         }
         return nums[l];
     }
