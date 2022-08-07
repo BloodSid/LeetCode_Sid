067b6ccf62fe7597d99f073cd3dad4fd4ac82d0f
89228 <892284811@qq.com>
2022-08-07 11:25:46 +0800

    第 305 场周赛 T4 最长理想子序列 优化 AC 24ms

--- contest/src/main/java/Contest0807/T4/Solution.java
+++ contest/src/main/java/Contest0807/T4/Solution.java
@@ -8,18 +8,19 @@ public class Solution {
     public int longestIdealString(String s, int k) {
         char[] ch = s.toCharArray();
         int n = ch.length;
-        int[][] dp = new int[26][n];
-        dp[ch[0] - 'a'][0] = 1;
-        for (int i = 0; i < n - 1; i++) {
-            int next = ch[i + 1] - 'a';
-            for (int j = 0; j < 26; j++) {
-                if (Math.abs(j - next) <= k) dp[next][i + 1] = Math.max(dp[next][i + 1], dp[j][i] + 1);
-                if (j != next) dp[j][i + 1] = dp[j][i];
+        int[] dp = new int[26];
+        dp[ch[0] - 'a'] = 1;
+        for (int i = 1; i < n; i++) {
+            int cur = ch[i] - 'a';
+            int t = 0;
+            for (int p = Math.max(0, cur - k); p <= Math.min(25, cur + k); p++) {
+                t = Math.max(t, dp[p]);
             }
+            dp[cur] = t + 1;
         }
         int max = 0;
         for (int i = 0; i < 26; i++) {
-            max = Math.max(max, dp[i][n - 1]);
+            max = Math.max(max, dp[i]);
         }
         return max;
     }
