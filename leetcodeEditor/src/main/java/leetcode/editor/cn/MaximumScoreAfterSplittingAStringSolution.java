647619524dd70ee91a5fba602da65e316bb3b53f
89228 <892284811@qq.com>
2022-08-13 23:06:15 +0800

    1422. 分割字符串的最大得分 枚举分割点 一次遍历 AC 1ms

--- leetcodeEditor/src/main/java/leetcode/editor/cn/MaximumScoreAfterSplittingAStringSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/MaximumScoreAfterSplittingAStringSolution.java
@@ -57,14 +57,15 @@ class Solution {
         char[] ch = s.toCharArray();
         int dp = 0;
         int n = ch.length;
-        for (int i = 0; i < n; i++) dp += ch[i] - '0';
-        int max = 0;
-        for (int i = 0; i < n - 1; i++) {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            sum += ch[i] - '0';
             if (ch[i] == '0') dp++;
             else dp--;
-            max = Math.max(max, dp);
+            if (i < n - 1) max = Math.max(max, dp);
         }
-        return max;
+        return max + sum;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
