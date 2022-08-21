3c70a6fdfc2fa9017f7c78afecf2642e49ddf72c
89228 <892284811@qq.com>
2022-08-21 12:53:53 +0800

    周赛T4 优先队列 不重不漏地生成所有子序列 + 负数转化为相反数 + 最小和变最大和 AC 64ms

--- contest/src/main/java/Contest0821/T4/Solution.java
+++ contest/src/main/java/Contest0821/T4/Solution.java
@@ -10,35 +10,36 @@ import java.util.PriorityQueue;
  */
 public class Solution {
     public long kSum(int[] nums, int k) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        for (int i = 0, j = n - 1; i < j; i++, j--) {
-            int t = nums[i]; nums[i] = nums[j]; nums[j] = t;
-        }
-        PriorityQueue<long[]> pq = new PriorityQueue<>(Comparator.comparingLong((long[] o) -> o[0]).reversed());
+        long neg = 0;
         long sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] <= 0) {
-                break;
-            }
+        int n = nums.length;
+        // 负数转化为相反数，使数组中全都是正数
+        for (int i = 0; i < n; i++) {
             sum += nums[i];
-            pq.offer(new long[]{sum, i});
-        }
-        pq.offer(new long[]{-1, 0});
-        long[] poll = null;
-        for (int i = 0; i < k; i++) {
-            poll = pq.poll();
-            long s = poll[0], idx = poll[1];
-            if (idx < n - 1) {
-                pq.add(new long[]{s - nums[(int) idx] + nums[(int) (idx + 1)], idx + 1});
+            if (nums[i] < 0) {
+                neg += nums[i];
+                nums[i] = -nums[i];
             }
-            if (idx < n - 1 && nums[(int) (idx + 1)] <= 0) {
-                pq.add(new long[]{s + nums[(int) (idx + 1)], idx + 1});
-            }
-            System.out.println("i = " + i);
-            System.out.println("poll[0] = " + poll[0]);
-            System.out.println(Arrays.toString(poll));
         }
-        return poll[0];
+        // 排序
+        Arrays.sort(nums);
+        PriorityQueue<long[]> pq = new PriorityQueue<>(Comparator.comparingLong((long[] o) -> o[0]));
+        pq.offer(new long[]{nums[0], 0});
+        // 求最大的第k个 = 求最小的第k个，然后求它的补集
+        // k = 1 时的答案，也就是空集的补集
+        long res = sum - neg;
+        for (int i = 0; i < k - 1; i++) {
+            long[] poll = pq.poll();
+            // 求得集合 S, 一个负数的相反数若在集合 S 里，则这个负数不在答案集合里；
+            // 若负数的相反数不在集合 S 里，则这个负数在答案集合里，
+            // 也就是 sum(S) + neg 可得对应的答案集合，再求补集，最大的第k个子序列
+            res = sum - (poll[0] + neg);
+            // 不重不漏地生成所有子序列。每次放进去的数不小于拿出来的数。
+            if (poll[1] == n - 1) continue;
+            int j = (int) poll[1];
+            pq.offer(new long[]{poll[0] + nums[j + 1], j + 1});
+            pq.offer(new long[]{poll[0] - nums[j] + nums[j + 1], j + 1});
+        }
+        return res;
     }
 }
