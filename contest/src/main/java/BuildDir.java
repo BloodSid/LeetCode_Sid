28d37ce800bbe871601cad75a3130ecbfb6329d1
89228 <892284811@qq.com>
2022-08-28 11:38:50 +0800

    竞赛目录构建工具升级：多场竞赛时自动递增命名

--- contest/src/main/java/BuildDir.java
+++ contest/src/main/java/BuildDir.java
@@ -13,8 +13,15 @@ public class BuildDir {
 
     public static void main(String[] args) {
         String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("MMdd"));
+        String contest = "contest/src/main/java/Contest" + date;
+        int idx = 0;
+        String suffix = "";
+        while(new File(contest + suffix).exists()) {
+            idx++;
+            suffix = "_" + idx;
+        }
         for (int i = 1; i <= T; i++) {
-            String dir = "contest/src/main/java/Contest" + date + "/T" + i;
+            String dir = contest + suffix + "/T" + i;
             System.out.println(dir);
             File file = new File(dir);
             file.mkdirs();
