a8974c5839a0733916e4a137a9232ad5158949c1
89228 <892284811@qq.com>
2022-09-03 03:49:16 +0800

    646. 最长数对链 贪心 AC 8ms 时间复杂度 O(nlogn)

--- leetcodeEditor/src/main/java/leetcode/editor/cn/MaximumLengthOfPairChainSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/MaximumLengthOfPairChainSolution.java
@@ -38,19 +38,15 @@ public class MaximumLengthOfPairChainSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int findLongestChain(int[][] pairs) {
-        Arrays.sort(pairs, Comparator.comparingInt(pair -> pair[0]));
-        int n = pairs.length;
-        int[] dp = new int[n];
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            int temp = 0;
-            for (int j = 0; j < i; j++) {
-                if (pairs[j][1] < pairs[i][0]) temp = Math.max(temp, dp[j]);
+        Arrays.sort(pairs, (p1, p2) -> p1[1] - p2[1]);
+        int cur = Integer.MIN_VALUE, cnt = 0;
+        for (int[] pair : pairs) {
+            if (cur < pair[0]) {
+                cur = pair[1];
+                cnt++;
             }
-            dp[i] = temp + 1;
-            max = Math.max(max, dp[i]);
         }
-        return max;
+        return cnt;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
