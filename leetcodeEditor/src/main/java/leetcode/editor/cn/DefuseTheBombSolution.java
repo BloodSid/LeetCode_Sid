c2237c09a576d67fc704a0bdcac12a0da60b1cdd
89228 <892284811@qq.com>
2022-09-24 00:39:37 +0800

    1652. 拆炸弹 枚举 AC 1ms O(n^2), 如果用滑动窗口做，可以做到 O(n)

--- leetcodeEditor/src/main/java/leetcode/editor/cn/DefuseTheBombSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/DefuseTheBombSolution.java
@@ -65,6 +65,19 @@ class Solution {
     public int[] decrypt(int[] code, int k) {
         int n = code.length;
         int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            int start, end;
+            if (k >= 0) {
+                start = i + 1;
+                end = i + k + 1;
+            } else {
+                start = i + k;
+                end = i;
+            }
+            for (; start < end; start++) {
+                res[i] += code[(start + n) % n];
+            }
+        }
         return res;
     }
 }
