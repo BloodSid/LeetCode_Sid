7a55a4d31b38d0a5e054aa0b35e91a78658c782a
89228 <892284811@qq.com>
2022-09-28 16:19:54 +0800

    面试题 17.09. 第 k 个数 初始化

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/GetKthMagicNumberLcciSolution.java
@@ -0,0 +1,32 @@
+package leetcode.editor.cn;
+//有些数的素因子只有 3，5，7，请设计一个算法找出第 k 个数。注意，不是必须有这些素因子，而是必须不包含其他的素因子。例如，前几个数按顺序应该是 1，3，
+//5，7，9，15，21。 
+//
+// 示例 1: 
+//
+// 输入: k = 5
+//
+//输出: 9
+// 
+// 👍 100 👎 0
+
+
+import java.util.*;
+
+/**
+ * 第 k 个数
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2022-09-28 00:51:28 
+ */
+public class GetKthMagicNumberLcciSolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int getKthMagicNumber(int k) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
