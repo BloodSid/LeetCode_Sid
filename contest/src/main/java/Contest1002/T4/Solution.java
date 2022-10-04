4cfb9cfee05d13cdd675fe96f02b3a6b498cbe55
89228 <892284811@qq.com>
2022-10-04 18:55:55 +0800

    第 313 场周赛 T4 对字母串可执行的最大删除数 动态规划 通过计算出最长公共后缀来优化每次判断状态转移的代价 AC 144ms 时间复杂度 O(n^2)

--- contest/src/main/java/Contest1002/T4/Solution.java
+++ contest/src/main/java/Contest1002/T4/Solution.java
@@ -1,7 +1,5 @@
 package Contest1002.T4;
 
-import java.util.HashMap;
-
 /**
  * @author IronSid
  * @since 2022-10-02 10:36
@@ -10,24 +8,26 @@ public class Solution {
     public int deleteString(String s) {
         char[] ch = s.toCharArray();
         int n = ch.length;
-        HashMap<Character, Integer> f = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            f.put(ch[i], f.getOrDefault(ch[i], 0) + 1);
-        }
-        if (f.size() == 1) return n;
+        // dp[i] 表示删至剩余 s[i:] 的最大步数
         int[] dp = new int[n];
+        //  lcp[i][j] 表示 s[i:] 和 s[j:] 的最长公共后缀
+        int[][] lcp = new int[n + 1][n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j > i; j--) {
+                if (ch[i] == ch[j]) lcp[i][j] = lcp[i + 1][j + 1] + 1;
+            }
+        }
         int maxD = 0;
         for (int j = 0; j < n; j++) {
             for (int i = Math.max(0, 2 * j - n); i < j; i++) {
+                // 如果没有办法通过若干步操作达到剩余 s[i:] 的状态，则跳过
                 if (i != 0 && dp[i] == 0) continue;
-                int k;
-                for (k = 0; k < j - i; k++) {
-                    if (ch[i + k] != ch[j + k]) break;
-                }
-                if (k == j - i) dp[j] = Math.max(dp[j], dp[i] + 1);
+                // s[i:] 和 s[j:] 有相同前缀，表示从 dp[i] 到 dp[j] 的状态转移成立
+                if (lcp[i][j] >= j - i) dp[j] = Math.max(dp[j], dp[i] + 1);
             }
             maxD = Math.max(maxD, dp[j]);
         }
+        // 任何位置都可以一步操作删除整个字符串，所以找出步数最大的位置再操作一步就是结果
         return maxD + 1;
     }
 }
