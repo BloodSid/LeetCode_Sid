1218c8dbb969f3b9fc5cd718e4e0166ba8e84707
89228 <892284811@qq.com>
2022-10-09 01:21:47 +0800

    2416. 字符串的前缀分数和 字典树 dfs AC 182ms 静态数组存树

--- contest/src/main/java/Contest0918/T4/Solution.java
+++ contest/src/main/java/Contest0918/T4/Solution.java
@@ -1,6 +1,8 @@
 package Contest0918.T4;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author IronSid
@@ -11,35 +13,46 @@ public class Solution {
     static int[][] tr = new int[N][26];
     static int[] cnt = new int[N];
 
-    void insert(String s) {
+    // 记录在该节点上终止的单词 id
+    static List<Integer>[] ids = new List[N];
+    private String[] words;
+    private int[] res;
+
+    void insert(int i) {
         int p = 0;
-        for (char c : s.toCharArray()) {
+        for (char c : words[i].toCharArray()) {
             int u = c - 'a';
             if (tr[p][u] == 0) tr[p][u] = ++idx;
             p = tr[p][u];
             cnt[p]++;
         }
+        if (ids[p] == null) ids[p] = new ArrayList<>();
+        ids[p].add(i);
     }
 
-    int query(String s) {
-        int p = 0;
-        int score = 0;
-        for (char c : s.toCharArray()) {
-            int u = c - 'a';
-            p = tr[p][u];
-            score += cnt[p];
+    void dfs(int cur, int sum) {
+        sum += cnt[cur];
+        List<Integer> id = ids[cur];
+        if (id != null) {
+            for (Integer i : id) res[i] += sum;
         }
-        return score;
+        for (int next : tr[cur]) {
+            if (next != 0) dfs(next, sum);
+        }
     }
+
     public int[] sumPrefixScores(String[] words) {
+        this.words = words;
         for (int i = 0; i <= idx; i++) {
             Arrays.fill(tr[i], 0);
         }
         Arrays.fill(cnt, 0, idx + 1, 0);
+        Arrays.fill(ids, 0, idx + 1, null);
         idx = 0;
-        for (String word : words) insert(word);
-        int[] res = new int[words.length];
-        for (int i = 0; i < words.length; i++) res[i] = query(words[i]);
+        for (int i = 0; i < words.length; i++) insert(i);
+        res = new int[words.length];
+        dfs(0, 0);
         return res;
     }
+
 }
