b3a28bfa2581222b92a3ab06d088c8eaba10c0d3
89228 <892284811@qq.com>
2022-10-22 20:52:29 +0800

    221021天池-04. 意外惊喜 动态规划 前缀和 分组背包 TLE 用了和 第 286 场周赛 “2218. 从栈中取出 K 个硬币的最大面值和” 一样的做法 有时候可以AC, 但是时间复杂度是超出要求的

--- contest/src/main/java/Contest1022/T4/Solution.java
+++ contest/src/main/java/Contest1022/T4/Solution.java
@@ -1,29 +1,31 @@
 package Contest1022.T4;
 
-import java.util.Arrays;
-
 /**
  * @author IronSid
  * @since 2022-10-22 15:03
  */
 public class Solution {
     public int brilliantSurprise(int[][] present, int limit) {
-        int[] dp = new int[limit + 1];
-        int[] ndp = new int[limit + 1];
-        int bound = 0;
-        for (int[] p : present) {
-            int n = p.length;
-            bound += n;
-            Arrays.fill(ndp, 0);
-            int l = Math.min(limit, bound);
-            for (int i = 0, t = 0; i <= l; i++) {
-                ndp[i] = Math.max(dp[i], dp[l - i] + t);
-                if (i < n) t += p[i];
+        int m = present.length;
+        int[][] dp = new int[m + 1][limit + 1];
+        int sumN = 0;
+        for (int i = 1; i <= m; i++) {
+            int[] pre = present[i - 1];
+            int n = pre.length;
+            int[] v = new int[n + 1];
+            for (int i1 = 0, t = 0; i1 < n; i1++) {
+                t += pre[i1];
+                v[i1 + 1] = t;
             }
-            int[] t = dp;
-            dp = ndp;
-            ndp = t;
+            sumN += n;
+            for (int j = 1; j <= Math.min(limit, sumN); j++) {
+                int max = 0;
+                for (int p = 0; p <= n && p <= j; p++) {
+                    max = Math.max(max, dp[i - 1][j - p] + v[p]);
+                }
+                dp[i][j] = max;
+            }
         }
-        return dp[limit];
+        return dp[m][limit];
     }
 }
