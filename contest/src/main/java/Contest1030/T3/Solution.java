a4cfb73035c5b3dda3dcde04177a2452b9121f33
89228 <892284811@qq.com>
2022-10-30 12:47:43 +0800

    第 317 场周赛 T3 美丽整数的最小增量 贪心 AC 简化逻辑

--- contest/src/main/java/Contest1030/T3/Solution.java
+++ contest/src/main/java/Contest1030/T3/Solution.java
@@ -6,36 +6,42 @@ package Contest1030.T3;
  */
 public class Solution {
     public long makeIntegerBeautiful(long n, int target) {
-        char[] s = Long.toString(n).toCharArray();
+        char[] cs = Long.toString(n).toCharArray();
+        int[] s = new int[cs.length];
         for (int i = 0; i < s.length; i++) {
-            s[i] -= '0';
+            s[i] = cs[i] - '0';
         }
-        boolean longer = false;
         int sum = 0;
-        for (char c : s) {
+        for (int c : s) {
             sum += c;
         }
-        int j = s.length - 1;
-        while (sum > target) {
-            s[j] = 0;
-            if (j > 0) s[j - 1]++;
-            else longer = true;
-            for (int k = j - 1; k >= 0; k--) {
-                if (s[k] != 10) break;
-                s[k] = 0;
-                if (k > 0) s[k - 1]++;
-                else longer = true;
+        if (sum <= target) return 0;
+        int p = -1;
+        int pre = 0;
+        // [0,p] 不变，剩下的位置变零
+        for (int i = 0; i < s.length; i++) {
+            pre += s[i];
+            if (pre + 1 <= target) {
+                p = i;
+            } else {
+                break;
             }
-            sum = longer ? 1 : 0;
-            for (char c : s) {
-                sum += c;
+        }
+        if (p >= 0) s[p]++;
+        for (int i = p + 1; i < s.length; i++) {
+            s[i] = 0;
+        }
+        for (int i = p; i >= 0; i--) {
+            if (s[i] == 10) {
+                s[i] = 0;
+                if (i == 0) p = -1;
+                else s[i - 1]++;
             }
-            j--;
         }
         StringBuilder sb = new StringBuilder();
-        if (longer) sb.append(1);
-        for (int p = 0; p < s.length; p++) {
-            sb.append((int) s[p]);
+        if (p == -1) sb.append(1);
+        for (int i = 0; i < s.length; i++) {
+            sb.append(s[i]);
         }
         return Long.parseLong(sb.toString()) - n;
     }
