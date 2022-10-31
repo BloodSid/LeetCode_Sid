d3467616c57f12cccbcdc1c1a055f4542196086e
89228 <892284811@qq.com>
2022-10-31 10:30:49 +0800

    第 317 场周赛 T3 美丽整数的最小增量 贪心 AC 0ms 简化逻辑 用数字运算代替数组模拟运算

--- contest/src/main/java/Contest1030/T3/Solution.java
+++ contest/src/main/java/Contest1030/T3/Solution.java
@@ -6,43 +6,14 @@ package Contest1030.T3;
  */
 public class Solution {
     public long makeIntegerBeautiful(long n, int target) {
-        char[] cs = Long.toString(n).toCharArray();
-        int[] s = new int[cs.length];
-        for (int i = 0; i < s.length; i++) {
-            s[i] = cs[i] - '0';
-        }
-        int sum = 0;
-        for (int c : s) {
-            sum += c;
-        }
-        if (sum <= target) return 0;
-        int p = -1;
-        int pre = 0;
-        // [0,p] 不变，剩下的位置变零
-        for (int i = 0; i < s.length; i++) {
-            pre += s[i];
-            if (pre + 1 <= target) {
-                p = i;
-            } else {
-                break;
+        long mod = 1;
+        for (long t = n; ; mod *= 10) {
+            t = t + (mod - t % mod) % mod;
+            int sum = 0;
+            for (long s = t; s > 0; s /= 10) {
+                sum += s % 10;
             }
+            if (sum <= target) return t - n;
         }
-        if (p >= 0) s[p]++;
-        for (int i = p + 1; i < s.length; i++) {
-            s[i] = 0;
-        }
-        for (int i = p; i >= 0; i--) {
-            if (s[i] == 10) {
-                s[i] = 0;
-                if (i == 0) p = -1;
-                else s[i - 1]++;
-            }
-        }
-        StringBuilder sb = new StringBuilder();
-        if (p == -1) sb.append(1);
-        for (int i = 0; i < s.length; i++) {
-            sb.append(s[i]);
-        }
-        return Long.parseLong(sb.toString()) - n;
     }
 }
