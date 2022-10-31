9da7617ebc52e292d0bc5de459f041c9d9d0898b
89228 <892284811@qq.com>
2022-10-31 22:57:43 +0800

    第 90 场双周赛 T4 下一个更大元素 IV 排序 + 有序列表 AC 214ms 用平衡树模拟有序列表

--- contest/src/main/java/Contest1029/T4/Solution.java
+++ contest/src/main/java/Contest1029/T4/Solution.java
@@ -1,6 +1,9 @@
 package Contest1029.T4;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.TreeSet;
+import java.util.stream.IntStream;
 
 /**
  * @author IronSid
@@ -11,25 +14,22 @@ public class Solution {
         int n = nums.length;
         // 记录下两个更大元素的位置
         int[] next = new int[n];
-        Deque<Integer> t = new ArrayDeque<>();
-        Deque<Integer> s = new ArrayDeque<>();
-        for (int i = 0; i < nums.length; i++) {
-            int cur = nums[i];
-            while (!s.isEmpty() && nums[s.peek()] < cur) {
-                next[s.pop()] = cur;
+        Integer[] k = IntStream.range(0, n).boxed().toArray(Integer[]::new);
+        // 按元素的大小顺序，元素的位置逆序依次排列
+        Arrays.sort(k, (p, q) -> nums[p] == nums[q] ? p - q : nums[q] - nums[p]);
+        // 存元素的位置
+        TreeSet<Integer> list = new TreeSet<>();
+        for (Integer i : k) {
+            Iterator<Integer> it = list.tailSet(i, false).iterator();
+            Integer j = null;
+            int p = 0;
+            while (p < 2 && it.hasNext()) {
+                j = it.next();
+                p++;
             }
-            List<Integer> temp = new ArrayList<>();
-            while (!t.isEmpty() && nums[t.peek()] < cur) {
-                temp.add(t.pop());
-            }
-            t.push(i);
-            // 把 t 中一次出栈的元素保持原有顺序加入 s
-            for (int j = temp.size() - 1; j >= 0; j--) {
-                s.push(temp.get(j));
-            }
+            next[i] = p < 2 ? -1 : nums[j];
+            list.add(i);
         }
-        for (Integer i : t) next[i] = -1;
-        for (Integer i : s) next[i] = -1;
         return next;
     }
 }
