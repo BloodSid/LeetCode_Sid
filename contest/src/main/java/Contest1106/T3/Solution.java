dfe68b5feded483ae825c85696ec042a5eebcecd
89228 <892284811@qq.com>
2022-11-06 20:41:44 +0800

    第 318 场周赛 T3 雇佣 K 位工人的总代价 堆 双指针 AC 99ms

--- contest/src/main/java/Contest1106/T3/Solution.java
+++ contest/src/main/java/Contest1106/T3/Solution.java
@@ -1,6 +1,7 @@
 package Contest1106.T3;
 
-import java.util.TreeSet;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * @author IronSid
@@ -11,26 +12,33 @@ public class Solution {
         long tot = 0L;
         int n = costs.length;
         // 存储可用的工人，代价升序，下标升序排列
-        TreeSet<Integer> list = new TreeSet<>((o1, o2) -> costs[o1] == costs[o2] ? o1 - o2 : costs[o1] - costs[o2]);
+        Comparator<Integer> comparator = (o1, o2) -> costs[o1] == costs[o2] ? o1 - o2 : costs[o1] - costs[o2];
+        PriorityQueue<Integer> l = new PriorityQueue<>(comparator);
+        PriorityQueue<Integer> r = new PriorityQueue<>(comparator);
         // 初始化
         for (int i = 0; i < candidates; i++) {
-            list.add(i);
+            l.offer(i);
         }
-        for (int i = n - candidates; i < n; i++) {
-            list.add(i);
+        // 只入一个堆，不重复入堆
+        for (int i = Math.max(candidates, n - candidates); i < n; i++) {
+            r.offer(i);
         }
         int left = candidates;
         int right = n - 1 - candidates;
         for (int i = 0; i < k; i++) {
-            int cur = list.first();
-            list.remove(cur);
+            // 判断从哪个堆中取
+            PriorityQueue<Integer> q;
+            if (l.isEmpty()) q = r;
+            else if (r.isEmpty()) q = l;
+            else q = costs[l.peek()] <= costs[r.peek()] ? l : r;
+            int cur = q.poll();
             tot += costs[cur];
             // 还有未入 list 的工人，判断这次选择的工人在哪边，从对应边加入一个工人到 list
             if (left <= right) {
                 if (cur < left) {
-                    list.add(left++);
+                    l.offer(left++);
                 } else {
-                    list.add(right--);
+                    r.offer(right--);
                 }
             }
         }
