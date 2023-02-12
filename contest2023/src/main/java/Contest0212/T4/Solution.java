682be6912048847f11d4551d2b03fe94f440f393
89228 <892284811@qq.com>
2023-02-12 12:43:13 +0800

    第 332 场周赛 T4 最少得分子序列 枚举 前后缀分解 AC 2ms 计算前缀和后缀的最长子序列，然后得出删除长度的最小值

--- contest2023/src/main/java/Contest0212/T4/Solution.java
+++ contest2023/src/main/java/Contest0212/T4/Solution.java
@@ -1,67 +1,30 @@
 package Contest0212.T4;
 
-import java.util.Arrays;
-
 /**
  * @author IronSid
  * @since 2023-02-12 10:29
  */
 public class Solution {
-    public int minimumScore(String s, String t) {
-        int[] map = new int[t.length()];
-        Arrays.fill(map, -1);
-        if (isSubsequent(s, t, map)) {
-            return 0;
+    public int minimumScore(String ss, String ts) {
+        char[] s = ss.toCharArray();
+        char[] t = ts.toCharArray();
+        int min = t.length;
+        // s 长度 i 的前缀对应的最长的 t 的前缀的结尾下标。对应指后者是前者的子序列
+        int[] preM = new int[s.length + 1];
+        for (int i = 0, j = 0; i < s.length; i++) {
+            if (j < t.length && s[i] == t[j]) {
+               j++;
+           }
+            preM[i + 1] = j;
         }
-        // 枚举是 s 的子序列的 t 的前缀，二分找到是 s 后半部分的子序列的 t 的后缀的最长长度，记录并找到两缀长度之和的最大值
-        int max = 0;
-        char[] sc = s.toCharArray();
-        char[] tc = t.toCharArray();
-        for (int i = -1; i < map.length; i++) {
-            if (i >= 0 && map[i] == -1) break;
-            int sRight = i == -1 ? 0 : map[i] + 1;
-            // t[:i+1] 对应 s[:map[i]+1]
-            int l = 0, r = t.length() - i - 1;
-            while (l <= r) {
-                int mid = l + r >> 1;
-                // 判断 t 长度 mid 的后缀 t[n-mid:]是不是 s[map[i]+1:] 的子序列
-                boolean flag = true;
-                int j1 = sRight;
-                for (int i1 = t.length() - mid; i1 < t.length(); i1++) {
-                    char c = tc[i1];
-                    for (; j1 < s.length(); j1++) {
-                        if (sc[j1] == c) break;
-                    }
-                    if (j1 == s.length()) {
-                        flag = false;
-                        break;
-                    }
-                    j1++;
-                }
-                if (flag) {
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
-                }
+        // 枚举 s 的后缀找对应的最长的 t 的后缀
+        for (int i = s.length - 1, j = t.length - 1; i >= 0; i--) {
+            if (j >= 0 && s[i] == t[j]) {
+                j--;
             }
-            // 最长可用长度是 r
-            max = Math.max(max, r + i + 1);
+            min = Math.min(min, j - preM[i] + 1);
         }
-        return t.length() - max;
-    }
-
-    // map 记录 t 的前缀对应的 s 的前缀的下标
-    boolean isSubsequent(String s, String sub, int[] map) {
-        int j = 0;
-        for (int i = 0; i < sub.length(); i++) {
-            char c = sub.charAt(i);
-            for (; j < s.length(); j++) {
-                if (s.charAt(j) == c) break;
-            }
-            if (j == s.length()) return false;
-            map[i] = j;
-            j++;
-        }
-        return true;
+        min = Math.min(min, t.length - preM[s.length]);
+        return Math.max(0, min);
     }
 }
