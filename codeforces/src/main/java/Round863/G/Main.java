3c8d1e5f7a82fbde5f9b676980bed930552f6e6f
dexuan <dexuan.xu@transwarp.cn>
2023-04-06 13:10:51 +0800

    Codeforces Round 863 (Div. 3) G Vlad and the Nice Paths 动态规划 G1 AC G2 WA 优化时间复杂度，但是存在过不了的用例

--- codeforces/src/main/java/Round863/G/Main.java
+++ codeforces/src/main/java/Round863/G/Main.java
@@ -3,7 +3,8 @@ package Round863.G;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Scanner;
+import java.math.BigInteger;
+import java.util.*;
 
 public class Main {
     public static final int MOD = (int) (1e9 + 7);
@@ -28,46 +29,101 @@ public class Main {
         int m = n / k * k;
         // f(i, j) 颜色为i，长度为j的符合要求的子序列
         long[][] f = new long[n + 1][m + 1];
+        // sum[j] 长度为j的子序列
+        long[] sum = new long[m + 1];
         for (int i = 1; i <= n; i++) {
             f[i][0] = 1;
         }
+        sum[0] = 1;
         int maxP = 0;
         for (int t = 0; t < n; t++) {
             int cur = c[t];
             for (int j = m; j >= 2; j--) {
                 if (j % k == 1) {
-                    long sum = 0;
-                    for (int i = 1; i <= n; i++) {
-                        sum += f[i][j - 1];
-                    }
-                    f[cur][j] = (f[cur][j] + sum) % MOD;
+                    f[cur][j] = (f[cur][j] + sum[j - 1]) % MOD;
+                    sum[j] = (sum[j] + sum[j - 1]) % MOD;
                 } else {
                     f[cur][j] = (f[cur][j] + f[cur][j - 1]) % MOD;
+                    sum[j] = (sum[j] + f[cur][j - 1]) % MOD;
                 }
                 if (j % k == 0 && f[cur][j] > 0) maxP = Math.max(maxP, j);
             }
             f[cur][1]++;
-        }
-        long res = 0;
-        for (int i = 1; i <= n; i++) {
-            res += f[i][maxP];
+            sum[1]++;
         }
         if (maxP == 0) {
             System.out.println(1);
         } else {
-            System.out.println(res % MOD);
+            System.out.println(sum[maxP] % MOD);
         }
     }
 }
 
 class Test {
     public static void main(String[] args) throws IOException {
-        String input = "1\n" +
-                "6 1\n" +
-                "3 3 3 2 6 5";
+        StringBuilder sb  = new StringBuilder();
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
+        int k = 50;
+        int m = (int) (1e9+7);
+        int rem = m;
+        int color = 1;
+        for (int n = k + 10; n >= k; n--) {
+            int i = 0;
+            while ((i + 1) * C(n, k) <= rem) {
+                i += 1;
+            }
+            if (i != 0) {
+                rem -= i * C(n, k);
+                for (int i1 = 0; i1 < i; i1++) {
+                    for (int i2 = 0; i2 < (n / 2); i2++) {
+                        a.add(color);
+                    }
+                    for (int i2 = 0; i2 < ((n + 1) / 2); i2++) {
+                        b.add(color);
+                    }
+                    color++;
+                }
+            }
+        }
+        for (int i = b.size() - 1; i >= 0; i--) {
+            a.add(b.get(i));
+        }
+        for (int i = 0, t = 4000 - a.size(); i < t; i++) {
+            a.add(color);
+        }
+        sb.append("1\n").append(a.size()).append(" ").append(k).append("\n");
+        for (Integer integer : a) {
+            sb.append(integer).append(' ');
+        }
+        System.out.println("test case:");
+        System.out.println(sb);
+        String input = sb.toString();
         InputStream is = new ByteArrayInputStream(input.getBytes());
         System.setIn(is);
+        long t = System.currentTimeMillis();
+        System.out.println("output:");
+        // 正确结果为0
         Main.main(null);
+        System.out.println("time cost:");
+        System.out.println((double) (System.currentTimeMillis() - t) / 1000 + "s");
+    }
 
+    static HashMap<String, Long> C = new HashMap<>();
+    static long C(int n, int k) {
+        C.computeIfAbsent(n + " " + k, t -> fac(n).divide(fac(n - k)).divide(fac(k)).longValueExact());
+        return C.get(n + " " + k);
+    }
+
+    static HashMap<Integer, BigInteger> fac = new HashMap<>();
+    static BigInteger fac(int n) {
+        fac.computeIfAbsent(n, t1 -> {
+            BigInteger t = BigInteger.valueOf(1);
+            for (int i = 2; i <= n; i++) {
+                t = t.multiply(BigInteger.valueOf(i));
+            }
+            return t;
+        });
+        return fac.get(n);
     }
 }
