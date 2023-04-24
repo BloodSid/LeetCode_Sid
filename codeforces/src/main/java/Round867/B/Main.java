e3e501c960fcde7ace9b0f6bcfa247123335f73c
89228 <892284811@qq.com>
2023-04-25 01:37:19 +0800

    Codeforces Round 867 (Div. 3) B Karina and Array 排序 模拟 AC 排序前需要打乱

--- codeforces/src/main/java/Round867/B/Main.java
+++ codeforces/src/main/java/Round867/B/Main.java
@@ -1,7 +1,9 @@
 package Round867.B;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
+
 
 /**
  * @author IronSid
@@ -9,6 +11,8 @@ import java.util.Scanner;
  */
 public class Main {
     static Scanner sc;
+    static Random random = new Random();
+
 
     public static void main(String[] args) {
         sc = new Scanner(System.in);
@@ -28,10 +32,25 @@ public class Main {
             System.out.println(a[0] * a[1]);
             return;
         }
+        shuffle(a);
         Arrays.sort(a);
         long max = Long.MIN_VALUE;
         max = Math.max(max, a[0] * a[1]);
         max = Math.max(max, a[n - 1] * a[n - 2]);
         System.out.println(max);
     }
+
+    public static void shuffle(long[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            swap(nums, i, i + random.nextInt(n - i));
+        }
+    }
+
+    private static void swap(long[] nums, int i, int j) {
+        long t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+
 }
