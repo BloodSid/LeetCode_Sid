966c1e616cdd7bed45c18c421944dc7af8e875e7
89228 <892284811@qq.com>
2023-04-27 03:42:31 +0800

    Codeforces Round 859 (Div. 4) D Odd Queries 前缀和 AC 尝试优化BufferedReader：一次性把所有行读入byte数组

--- codeforces/src/main/java/Round859/D/Solution.java
+++ codeforces/src/main/java/Round859/D/Solution.java
@@ -1,39 +1,56 @@
 package Round859.D;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 /**
  * @author IronSid
  * @since 2023-03-19 23:08
  */
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        for (int cases = 0; cases < N; cases++) {
-            String[] split = br.readLine().split(" ");
-            int n = Integer.parseInt(split[0]);
-            int q = Integer.parseInt(split[1]);
-            int[] a = new int[n];
-            String line = br.readLine();
-            String[] nums = line.split(" ");
-            for (int i = 0; i < nums.length; i++) {
-                a[i] = Integer.parseInt(nums[i]);
+    static byte[] buf = new byte[(int) 1e7];
+    static int cursor = 0;
+    static int length = 0;
+    static String next() {
+        int i;
+        for (i = cursor; i < length; i++) {
+            if (buf[i] == ' ' || buf[i] == '\r') {
+                break;
             }
-            long[] p = new long[n + 1];
-            long t = 0;
+        }
+        String s = new String(buf, cursor, i - cursor);
+        if (i < length) {
+            // \r\n 跳过两个字节
+            cursor = buf[i] == ' ' ? i + 1 : i + 2;
+        }
+        return s;
+    }
+    public static void main(String[] args) {
+        try {
+            length = System.in.read(buf);
+            buf[length] = ' ';
+            cursor = 0;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        int N = Integer.parseInt(next());
+        for (int cases = 0; cases < N; cases++) {
+            int n = Integer.parseInt(next());
+            int q = Integer.parseInt(next());
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(next()) % 2;
+            }
+            int[] p = new int[n + 1];
+            int t = 0;
             for (int i = 0; i < n; i++) {
                 t += a[i];
                 p[i + 1] = t;
             }
             for (int i = 0; i < q; i++) {
-                String[] query = br.readLine().split(" ");
-                int l = Integer.parseInt(query[0]);
-                int r = Integer.parseInt(query[1]);
-                int k = Integer.parseInt(query[2]) % 2;
-                if ((p[n] - p[r] + p[l - 1] + k * (r - l + 1)) % 2 == 1) {
+                int l = Integer.parseInt(next());
+                int r = Integer.parseInt(next());
+                int k = Integer.parseInt(next()) % 2;
+                if ((p[n] - p[r] + p[l - 1] + k * (r - l + 1) + 2) % 2 == 1) {
                     System.out.println("YES");
                 } else {
                     System.out.println("NO");
