cc070d2e8abdfd87f2928359f2972ee3574f086d
dexuan <dexuan.xu@transwarp.cn>
2023-04-27 20:00:11 +0800

    Codeforces Round 859 (Div. 4) D Odd Queries 前缀和 AC Kattio快读模板

--- codeforces/src/main/java/Round859/D/Solution.java
+++ codeforces/src/main/java/Round859/D/Solution.java
@@ -1,61 +1,83 @@
 package Round859.D;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.StringTokenizer;
 
 /**
  * @author IronSid
  * @since 2023-03-19 23:08
  */
 public class Solution {
-    static byte[] buf = new byte[(int) 1e7];
-    static int cursor = 0;
-    static int length = 0;
-    static String next() {
-        int i;
-        for (i = cursor; i < length; i++) {
-            if (buf[i] == ' ' || buf[i] == '\r') {
-                break;
-            }
-        }
-        String s = new String(buf, cursor, i - cursor);
-        if (i < length) {
-            // \r\n 跳过两个字节
-            cursor = buf[i] == ' ' ? i + 1 : i + 2;
-        }
-        return s;
-    }
     public static void main(String[] args) {
-        try {
-            length = System.in.read(buf);
-            buf[length] = ' ';
-            cursor = 0;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        int N = Integer.parseInt(next());
+        Kattio ka = new Kattio();
+        int N = ka.nextInt();
         for (int cases = 0; cases < N; cases++) {
-            int n = Integer.parseInt(next());
-            int q = Integer.parseInt(next());
+            int n = ka.nextInt();
+            int q = ka.nextInt();
             int[] a = new int[n];
             for (int i = 0; i < n; i++) {
-                a[i] = Integer.parseInt(next()) % 2;
+                a[i] = ka.nextInt() % 2;
             }
             int[] p = new int[n + 1];
             int t = 0;
             for (int i = 0; i < n; i++) {
-                t += a[i];
+                t ^= a[i];
                 p[i + 1] = t;
             }
             for (int i = 0; i < q; i++) {
-                int l = Integer.parseInt(next());
-                int r = Integer.parseInt(next());
-                int k = Integer.parseInt(next()) % 2;
+                int l = ka.nextInt();
+                int r = ka.nextInt();
+                int k = ka.nextInt() % 2;
                 if ((p[n] - p[r] + p[l - 1] + k * (r - l + 1) + 2) % 2 == 1) {
-                    System.out.println("YES");
+                    ka.println("YES");
                 } else {
-                    System.out.println("NO");
+                    ka.println("NO");
                 }
             }
+        }
+        ka.flush();
+    }
+
+    public static class Kattio extends PrintWriter {
+        private BufferedReader r;
+        private StringTokenizer st;
+
+        // 标准io
+        public Kattio() {
+            this(System.in, System.out);
+        }
+
+        public Kattio(InputStream i, OutputStream o) {
+            super(o);
+            r = new BufferedReader(new InputStreamReader(i));
+        }
+
+        // 文件io
+        public Kattio(String input, String output) throws IOException {
+            super(output);
+            r = new BufferedReader(new FileReader(input));
+        }
+
+        // 在没有其它输入时返回null
+        public String next() {
+            try {
+                while (st == null || !st.hasMoreTokens())
+                    st = new StringTokenizer(r.readLine());
+                return st.nextToken();
+            } catch (Exception e) {}
+            return null;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
         }
     }
 }
