c4287921985179ccc632f57a7b91e56693054fbd
89228 <892284811@qq.com>
2023-04-30 14:41:30 +0800

    第 343 场周赛 T4 字典序最小的美丽字符串 贪心 递推 AC 11ms

--- contest2023/src/main/java/Contest0430/T4/Solution.java
+++ contest2023/src/main/java/Contest0430/T4/Solution.java
@@ -1,58 +1,49 @@
 package Contest0430.T4;
 
-import java.util.Arrays;
-
 /**
  * @author IronSid
  * @since 2023-04-30 10:34
  */
 public class Solution {
 
-    private char[] res;
-    private char limit;
-    private char[] c;
-
     public String smallestBeautifulString(String s, int k) {
-        limit = (char) ('a' + k - 1);
-        char[] t = new char[s.length()];
-        Arrays.fill(t, (char) ('a' + k - 1));
-        for (int i = 0; i < t.length; i++) {
-            t[i] -= i % 3;
-        }
-        String max = new String(t);
-        if (s.compareTo(max) >= 0) return "";
+        char limit = (char) ('a' + k - 1);
         int n = s.length();
-        c = s.toCharArray();
-        res = new char[n];
-        // 找到第一个回文出现的位置
-        dfs(0, (char) 0, (char) 0);
-        // 空的位置全用abc填充
+        char[] c = s.toCharArray();
+        // 找到需要增加的位置
         int i;
         for (i = 0; i < n; i++) {
-            if (res[i] == 0) break;
+            if (c[i] > limit || (i >= 1 && c[i - 1] == c[i]) || (i >= 2 && c[i - 2] == c[i])) {
+                break;
+            }
         }
+        while (i >= 0) {
+            if (i == n) {
+                c[i - 1]++;
+                i--;
+                continue;
+            }
+            while ((i >= 1 && c[i - 1] == c[i]) || (i >= 2 && c[i - 2] == c[i])) {
+                c[i]++;
+            }
+            if (c[i] > limit) {
+                if (i >= 1) c[i - 1]++;
+                i--;
+            } else {
+                // 找到了不修改的字母
+                break;
+            }
+        }
+        if (i == -1) return "";
+        // c[i] 已经变大，c[i+1:]尽可能取小
+        i++;
         for (; i < n; i++) {
-            res[i] = 'a';
-            while ((i >= 1 && res[i - 1] == res[i]) || (i >= 2 && res[i - 2] == res[i])) {
-                res[i]++;
+            char ci = 'a';
+            while ((i >= 1 && c[i - 1] == ci) || (i >= 2 && c[i - 2] == ci)) {
+                ci++;
             }
+            c[i] = ci;
         }
-        return new String(res);
-    }
-
-    boolean dfs(int i, char p1, char p2) {
-        if (i == res.length) {
-            return false;
-        }
-        for (char a = c[i]; a <= limit; a++) {
-            if (a == p1 || a == p2) continue;
-            res[i] = a;
-            if (res[i] != c[i]) return true;
-            if (dfs(i + 1, p2, a)) {
-                return true;
-            }
-        }
-        res[i] = 0;
-        return false;
+        return new String(c);
     }
 }
