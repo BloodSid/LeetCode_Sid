79cc74dc34f0c7623fdf5829060f3a91c41ad7f5
89228 <892284811@qq.com>
2023-05-29 19:44:32 +0800

    373. 查找和最小的 K 对数字 最小堆 使用控制遍历方向避免重复访问简化了实现 AC 25ms

--- leetcodeEditor/src/main/java/leetcode/editor/cn/FindKPairsWithSmallestSumsSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/FindKPairsWithSmallestSumsSolution.java
@@ -12,32 +12,23 @@ import java.util.*;
 public class FindKPairsWithSmallestSumsSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    boolean reverse = false;
-
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        int len1 = nums1.length;
-        int len2 = nums2.length;
-        if (len1 > len2) {
-            reverse = true;
-            return kSmallestPairs(nums2, nums1, k);
-        }
-        List<List<Integer>> ans = new ArrayList<>(Math.min(len1 * len2, k));
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) ->
-                nums1[o1[0]] + nums2[o1[1]] - nums1[o2[0]] - nums2[o2[1]]);
-        for (int i = 0; i < Math.min(len1, k); i++) {
-            pq.offer(new int[]{i, 0});
-        }
-        while (ans.size() < k && !pq.isEmpty()) {
-            int[] cur = pq.poll();
-            List<Integer> list = new ArrayList<>();
-            if (reverse) {
-                Collections.addAll(list, nums2[cur[1]], nums1[cur[0]]);
-            } else {
-                Collections.addAll(list, nums1[cur[0]], nums2[cur[1]]);
+        int len1 = nums1.length, len2 = nums2.length;
+        int size = (int) Math.min((long) len1 * len2, k);
+        List<List<Integer>> ans = new ArrayList<>(size);
+        // {i1, i2, sum}
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[2] - b[2]));
+        pq.offer(new int[]{0, 0, nums1[0] + nums2[0]});
+        for (int i = 0; i < size; i++) {
+            int[] p = pq.poll();
+            int i1 = p[0], i2 = p[1];
+            ans.add(Arrays.asList(nums1[i1], nums2[i2]));
+            // 通过规定顺序避免重复访问
+            if (i1 == 0 && i2 < len2 - 1) {
+                pq.offer(new int[]{i1, i2 + 1, nums1[i1] + nums2[i2 + 1]});
             }
-            ans.add(list);
-            if (cur[1] + 1 < len2) {
-                pq.offer(new int[]{cur[0], cur[1] + 1});
+            if (i1 < len1 - 1) {
+                pq.offer(new int[]{i1 + 1, i2, nums1[i1 + 1] + nums2[i2]});
             }
         }
         return ans;
