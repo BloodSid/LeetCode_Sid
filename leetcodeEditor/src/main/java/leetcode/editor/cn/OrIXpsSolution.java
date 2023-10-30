81e425869339cb7925f30bd2a8aa504894436283
89228 <892284811@qq.com>
2023-10-30 12:19:49 +0800

    LCR 031. LRU 缓存 和146. LRU 缓存是同一题 使用LinkedHashMap 和AC 41ms

---
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/OrIXpsSolution.java
@@ -0,0 +1,96 @@
+package leetcode.editor.cn;
+//
+// 运用所掌握的数据结构，设计和实现一个 LRU (Least Recently Used，最近最少使用) 缓存机制 。 
+//
+// 实现 LRUCache 类： 
+//
+// 
+// LRUCache(int capacity) 以正整数作为容量 capacity 初始化 LRU 缓存 
+// int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。 
+// void put(int key, int value) 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。当缓存容量达到上
+//限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。 
+// 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入
+//["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+//[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+//输出
+//[null, null, null, 1, null, -1, null, -1, 3, 4]
+//
+//解释
+//LRUCache lRUCache = new LRUCache(2);
+//lRUCache.put(1, 1); // 缓存是 {1=1}
+//lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+//lRUCache.get(1);    // 返回 1
+//lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+//lRUCache.get(2);    // 返回 -1 (未找到)
+//lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+//lRUCache.get(1);    // 返回 -1 (未找到)
+//lRUCache.get(3);    // 返回 3
+//lRUCache.get(4);    // 返回 4
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= capacity <= 3000 
+// 0 <= key <= 10000 
+// 0 <= value <= 10⁵ 
+// 最多调用 2 * 10⁵ 次 get 和 put 
+// 
+// 
+//
+// 
+//
+// 进阶：是否可以在 O(1) 时间复杂度内完成这两种操作？ 
+//
+// 
+//
+// 注意：本题与主站 146 题相同：https://leetcode-cn.com/problems/lru-cache/ 
+// 👍 52 👎 0
+
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * 最近最少使用缓存
+ *
+ * @author IronSid
+ * @version 1.0
+ * @since 2022-06-28 13:31:11 
+ */
+public class OrIXpsSolution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class LRUCache extends LinkedHashMap<Integer, Integer> {
+
+    private final LinkedHashMap<Integer, Integer> map;
+
+    public LRUCache(int capacity)  {
+        map = new LinkedHashMap<Integer, Integer>(capacity, 0.75F, true){
+            final int cap = capacity;
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return size() > cap;
+            }
+        };
+    }
+
+    public int get(int key) {
+        Integer value = map.get(key);
+        return value == null ? -1 : value;
+    }
+
+    public void put(int key, int value) {
+        map.put(key, value);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
