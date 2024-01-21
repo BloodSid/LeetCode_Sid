ad7dcf2ee0f6a93c216ad5d664353e49ce695a3e
SidXu <892284811@qq.com>
2024-01-21 10:28:25 +0800

    2024年周赛目录

---
+++ contest2024/src/main/java/BuildDir.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author IronSid
+ * @since 2022-07-31 9:57
+ */
+public class BuildDir {
+
+    // 题目数
+    public static final int T = 4;
+
+    public static void main(String[] args) {
+        String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("MMdd"));
+        String year = String.valueOf(LocalDateTime.now().getYear());
+        String contest = "contest"+ year + "/src/main/java/Contest" + date;
+        int idx = 0;
+        String suffix = "";
+        while(new File(contest + suffix).exists()) {
+            idx++;
+            suffix = "_" + idx;
+        }
+        for (int i = 1; i <= T; i++) {
+            String dir = contest + suffix + "/T" + i;
+            System.out.println(dir);
+            File file = new File(dir);
+            file.mkdirs();
+        }
+    }
+}
