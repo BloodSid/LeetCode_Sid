16a1a201eea831e3189290799294ed8b9a5d9e08
SidXu <892284811@qq.com>
2024-02-21 14:26:46 +0800

    105. 从前序与中序遍历序列构造二叉树 迭代 栈 AC 1ms

--- leetcodeEditor/src/main/java/leetcode/editor/cn/ConstructBinaryTreeFromPreorderAndInorderTraversalSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/ConstructBinaryTreeFromPreorderAndInorderTraversalSolution.java
@@ -1,46 +1,86 @@
 package leetcode.editor.cn;
+//给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder 是同一棵树的中序遍历，请构造二叉树并
+//返回其根节点。 
+//
+// 
+//
+// 示例 1: 
+// 
+// 
+//输入: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+//输出: [3,9,20,null,null,15,7]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: preorder = [-1], inorder = [-1]
+//输出: [-1]
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= preorder.length <= 3000 
+// inorder.length == preorder.length 
+// -3000 <= preorder[i], inorder[i] <= 3000 
+// preorder 和 inorder 均 无重复 元素 
+// inorder 均出现在 preorder 
+// preorder 保证 为二叉树的前序遍历序列 
+// inorder 保证 为二叉树的中序遍历序列 
+// 
+//
+// 👍 2239 👎 0
+
 
 import binaryTree.TreeNode;
 
-import java.util.HashMap;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 /**
  * 从前序与中序遍历序列构造二叉树
  *
  * @author IronSid
  * @version 1.0
- * @since 2021-11-18 17:41:11
+ * @since 2024-02-20 21:47:18 
  */
 public class ConstructBinaryTreeFromPreorderAndInorderTraversalSolution {
+static
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    HashMap<Integer, Integer> hash;
-
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        int n = preorder.length;
-        hash = new HashMap<>(n);
-        for (int i = 0; i < n; i++) {
-            hash.put(inorder[i], i);
-        }
-        return buildTree(0, n - 1, preorder, 0, n - 1, inorder);
-    }
-
-    TreeNode buildTree(int pStart, int pEnd, int[] preorder, int iStart, int iEnd, int[] inorder) {
-        if (pStart > pEnd) {
+        if (preorder == null || preorder.length == 0) {
             return null;
         }
-        if (pStart == pEnd) {
-            return new TreeNode(preorder[pStart]);
+        // 栈，存放当前节点所有还没有考虑过右儿子的祖先节点。栈顶就是当前节点
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        // 根节点
+        TreeNode root = new TreeNode(preorder[0]);
+        stack.push(root);
+        // 指向 inorder 的下标，表示当前节点不断往左走达到的最终节点
+        int idx = 0;
+        for (int i = 1; i < preorder.length; i++) {
+            int val = preorder[i];
+            TreeNode newNode = new TreeNode(val);
+            TreeNode node = stack.peek();
+            // 栈顶和idx指向的元素不相等，说明栈顶节点有左儿子，且就是新节点
+            if (node.val != inorder[idx]) {
+                node.left = newNode;
+            } else {
+                // 栈顶和idx指向的元素相等，则向上回溯到第一个有右儿子的节点
+                while (!stack.isEmpty() && stack.peek().val == inorder[idx]) {
+                    idx++;
+                    node = stack.pop();
+                }
+                node.right = newNode;
+            }
+            // 当前节点入栈
+            stack.push(newNode);
         }
-        int rootVal = preorder[pStart];
-        int inorderRoot = hash.get(rootVal);
-        int leftSize = inorderRoot - iStart;
-        int rightSize = iEnd - inorderRoot;
-        TreeNode left = buildTree(pStart + 1, pStart + leftSize, preorder,
-                iStart, inorderRoot - 1, inorder);
-        TreeNode right = buildTree(pEnd - rightSize + 1, pEnd, preorder,
-                inorderRoot + 1, iEnd, inorder);
-        return new TreeNode(rootVal, left, right);
+        return root;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
