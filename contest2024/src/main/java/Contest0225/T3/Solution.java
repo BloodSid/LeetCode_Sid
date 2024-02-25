2b8e4063cbdb8e67d7ed1f2943f53573cf0dca91
SidXu <892284811@qq.com>
2024-02-25 12:49:21 +0800

    第 386 场周赛 T3 标记所有下标的最早秒数 I 二分 贪心 AC

--- contest2024/src/main/java/Contest0225/T3/Solution.java
+++ contest2024/src/main/java/Contest0225/T3/Solution.java
@@ -1,50 +1,54 @@
 package Contest0225.T3;
 
-import java.util.Arrays;
-
 /**
  * @author IronSid
  * @since 2024-02-25 10:31
  */
 public class Solution {
+    private int[] nums;
+    private int[] changeIndices;
+    private int n;
+
     public int earliestSecondToMarkIndices(int[] nums, int[] changeIndices) {
-        int n = nums.length;
+        this.nums = nums;
+        this.changeIndices = changeIndices;
+        n = nums.length;
         int m = changeIndices.length;
-        int[] last = new int[n];
-        for (int i = 0; i < m; i++) {
-            last[changeIndices[i] - 1] = i + 1;
+        int l = 0, r = m - 1;
+        while (l <= r) {
+            int mid = l + r >> 1;
+            if (check(mid)) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
         }
-        Integer[] idx = new Integer[n];
+        return l != m ? l + 1 : -1;
+    }
+
+    // 判断t秒能不能标记全部下标
+    boolean check(int t) {
+        boolean[] mark = new boolean[n];
+        long sum = 0;
+        // 从后往前扫，尽量在最后一次标记
+        for (int j = t; j >= 0; j--) {
+            // 贪心：在最迟的一次进行标记
+            int i = changeIndices[j] - 1;
+            if (!mark[i]) {
+                mark[i] = true;
+                sum += nums[i];
+            } else if (sum != 0) {
+                // 不进行标记且还有待做的减一操作，则进行减一操作
+                sum--;
+            }
+        }
         for (int i = 0; i < n; i++) {
-            idx[i] = i;
-        }
-        Arrays.sort(idx, (a, b) -> last[a] - last[b]);
-        // 某个下标i的 last[i]=0 表示该下标从未被标记
-        if (last[idx[0]] == 0) {
-            return -1;
-        }
-        int t = 0;
-        int sum = 0;
-        int p = 1;
-        // 贪心按照标记时间顺序进行标记
-        for (int j = 0; j < n; j++) {
-            int i = idx[j];
-            sum += nums[i] + 1;
-            if (sum > last[i]) {
-                return -1;
+            // 有未标记的元素
+            if (!mark[i]) {
+                return false;
             }
-            // 最早可以标记i的时间
-            p = Math.max(p, sum);
-            // 找最近的 changeIndices[p]=i
-            while (p <= m && changeIndices[p - 1] - 1 != i) {
-                p++;
-            }
-            // changeIndices 用尽
-            if (p == m + 1) {
-                return -1;
-            }
-            t = p;
         }
-        return t;
+        // 若sum大于零说明减一操作不足，不能全部标记
+        return sum <= 0;
     }
 }
