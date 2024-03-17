e08cab55be0f07ec07abafefeae0e8a0c548229b
SidXu <892284811@qq.com>
2024-03-17 16:25:23 +0800

    第 389 场周赛 T4 拾起 K 个 1 需要的最少行动次数 仓库选址问题 中位数贪心 AC 7ms

--- contest2024/src/main/java/Contest0317/T4/Solution.java
+++ contest2024/src/main/java/Contest0317/T4/Solution.java
@@ -1,5 +1,8 @@
 package Contest0317.T4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author IronSid
  * @since 2024-03-17 10:23
@@ -7,68 +10,47 @@ package Contest0317.T4;
 public class Solution {
     public long minimumMoves(int[] nums, int k, int mc) {
         int n = nums.length;
-        long[] p = new long[n + 1];
-        long[] q = new long[n + 1];
-        long tq = 0;
-        for (int i = 0, t = 0; i < n; i++) {
-            t += nums[i];
-            tq += (long) nums[i] * i;
-            p[i + 1] = t;
-            q[i + 1] = tq;
-        }
-        int[] idx = new int[(int) p[n]];
-        for (int i = 0, j = 0; i < n; i++) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 1) {
-                idx[j++] = i;
+                list.add(i);
             }
         }
-        // 滑动窗口计算最近的k-mc个1离当前位置的距离
-        long[] s = new long[n];
-        if (k - mc > 0) {
-            int cnt = k - mc;
-            int l = 0, r = cnt - 1;
-            for (int i = 0; i < n; i++) {
-                while (r < idx.length - 1 && Math.abs(idx[r + 1] - i) <= Math.abs(i - idx[l])) {
-                    l++;
-                    r++;
-                }
-                // [l,i) 和[i,r] 分别计算
-                s[i] = q[idx[r] + 1] - q[i] - (q[i] - q[idx[l]]) + (p[i] - p[idx[l]]) * i - (p[idx[r] + 1] - p[i]) * i;
-            }
-        }
-        // 特殊处理 k = 1, 2 的特殊情况
-        if (k == 1 && p[n] > 0) return 0;
-        if (k == 2) {
-            for (int i = 0; i < n - 1; i++) {
-                if (p[i + 2] - p[i] == 2) {
-                    return 1;
-                }
-            }
-        }
-        long res = Long.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            long sum = 0;
-            int cnt = 0;
-            // 优先考虑 i, i-1, i+1 上的1
-            if (i > 0 && nums[i - 1] == 1) {
-                sum += 1;
-                cnt++;
-            }
-            if (nums[i] == 1) {
-                cnt++;
-            }
-            if (i < n - 1 && nums[i + 1] == 1) {
-                sum += 1;
-                cnt++;
-            }
-            if (cnt + mc >= k) {
-                // mc 中的1够用。k等于1或2时 cnt > k 的情况已经排除
-                res = Math.min(res, sum + (k - cnt) * 2L);
+        int m = list.size();
+        int[] idx = new int[m];
+        long[] p = new long[m + 1];
+        long t = 0;
+        // c: 最多连续的1的个数
+        int c = 0;
+        for (int i = 0, tc = 0; i < m; i++) {
+            t += idx[i] = list.get(i);
+            p[i+1] = t;
+            if (i > 0 && idx[i] == idx[i - 1] + 1) {
+                tc++;
             } else {
-                // mc 中的1不够用，则找离i最近的 k-mc 个 1
-                res = Math.min(res, s[i] + mc * 2);
+                tc = 1;
             }
+            c = Math.max(c, tc);
         }
-        return res;
+        // 只考虑连续的3个
+        c = Math.min(c, 3);
+        // 分3步考虑，1. 选i, i+1, i-1 上的1; 2. 再接着选mc中的 3. 如果还不够，则选择离i最近的
+        if (k <= c) {
+            return k - 1;
+        }
+        if (k - c <= mc) {
+            return (k - c) * 2L + Math.max(c - 1, 0);
+        }
+        // 考虑到第3步，此时问题可转化成mc中的全选，然后选离i最近的k-mc个，可用中位数贪心求解
+        int size = k - mc;
+        long res = Long.MAX_VALUE;
+        for (int r = size; r <= m; r++) {
+            int l = r - size;
+            int i = l + size / 2;
+            long ls = (long) (i - l) * idx[i] - (p[i] - p[l]);
+            long rs = p[r] - p[i] - (long) (r - i) * idx[i];
+            res = Math.min(res, ls + rs);
+        }
+        return mc * 2L + res;
     }
 }
