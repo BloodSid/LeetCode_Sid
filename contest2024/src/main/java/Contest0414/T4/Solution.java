59485efa78d63640baa447ce0be6fdb63a7c648c
SidXu <892284811@qq.com>
2024-04-14 13:45:44 +0800

    第 393 场周赛 划分数组得到最小的值之和 动态规划 前缀数组 单调队列 WA 按照”最小的值之和“做单调队列，可能会把正确的转移排除掉。根本原因是，用更长且”值“更小的子数组排除”值“更大但是更短的贪心不成立。实际上，若对于 dp[i][j] 合法的起点 是 [l,r] 那么对于 dp[i+1][j], 根据与运算的性质，l可能会更小，r可能会更大（因为新数字二进制的0会让对应位置是1的数字可以加入子数组），所以边界的单调性不成立，不能用单调队列。

--- contest2024/src/main/java/Contest0414/T4/Solution.java
+++ contest2024/src/main/java/Contest0414/T4/Solution.java
@@ -1,7 +1,13 @@
 package Contest0414.T4;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
 
+/**
+ * @author IronSid
+ * @since 2024-04-14 10:27
+ */
 public class Solution {
 
     public static final int INF = (int) (1e9+7);
@@ -18,36 +24,55 @@ public class Solution {
                 p[i+1][j] = p[i][j] + (1 & (~nums[i] >> j));
             }
         }
-        int[][] dp = new int[n + 1][m + 1];
+        // dp(i,j)表示前i个元素划分得到v[0,j]的最小“值”之和
+        int[][] dp = new int[n + 1][m];
         for (int[] row : dp) {
             Arrays.fill(row, INF);
         }
-
+        // 单调队列-增，{sum子数组值之和，idx最后一个元素的下标}
+        Deque<int[]>[] q = new Deque[m];
+        for (int i = 0; i < m; i++) {
+            q[i] = new ArrayDeque<>();
+        }
         // 全1
         int add = -1;
         for (int i = 1; i <= n; i++) {
             add &= nums[i - 1];
-            if (add == v[0]) {
-                dp[i][1] = nums[i - 1];
-            }
-            for (int j = 2; j <= m; j++) {
-                int k = v[j - 1];
-                if ((nums[i - 1] & k) == k) {
-                    // 可以贴在上一个数组的后面
-                    dp[i][j] = Math.min(dp[i][j], (dp[i - 1][j] != INF ? (dp[i - 1][j] + nums[i - 1] - (i >= 2 ? nums[i - 2] : 0)) : INF));
+            for (int j = m - 1; j >= 0; j--) {
+                if (j == 0) {
+                    if (add == v[0]) {
+                        dp[i][0] = nums[i - 1];
+                    }
+                } else {
+                    int k = v[j];
+                    if ((nums[i - 1] & k) == k) {
+                        // 可以贴在上一个数组的后面
+                        if (dp[i - 1][j] != INF) {
+                            dp[i][j] = Math.min(dp[i][j], (dp[i - 1][j] + nums[i - 1] - nums[i - 2]));
+                        }
+                    }
+                    // 不贴在上一个数组的后面，二分找这个子数组最远和最近的起点范围
+                    int l = lowerBound(k, i - 1);
+                    int r = lowerBound(k + 1, i - 1) - 1;
+                    if (r < l) continue; // 没有合法的起点
+                    // 从队首排除不合法的转移
+                    while (!q[j - 1].isEmpty() && q[j - 1].peekFirst()[1] + 1 < l) {
+                        q[j - 1].pollFirst();
+                    }
+                    if (!q[j - 1].isEmpty() && q[j - 1].peekFirst()[1] + 1 <= r) {
+                        dp[i][j] = Math.min(dp[i][j], q[j - 1].peekFirst()[0] + nums[i - 1]);
+                    }
                 }
-                // 不贴在上一个数组的后面，二分找h的范围
-                int l = lowerBound(k, i - 1);
-                int r = lowerBound(k + 1, i - 1) - 1;
-                if (addRange(l, i - 1) != k) {
-                    continue;
-                }
-                for (int h = l; h <= r; h++) {
-                    dp[i][j] = Math.min(dp[i][j], dp[h][j - 1] + nums[i - 1]);
+                // 更新单调队列
+                if (dp[i][j] < INF) {
+                    while (!q[j].isEmpty() && q[j].peekLast()[0] > dp[i][j]) {
+                        q[j].pollLast();
+                    }
+                    q[j].offerLast(new int[]{dp[i][j], i - 1});
                 }
             }
         }
-        return dp[n][m] < INF ? dp[n][m] : -1;
+        return dp[n][m-1] < INF ? dp[n][m-1] : -1;
     }
 
     // target表示与运算目标值，end表示范围的上界，返回第一个与运算值大于等于target 的start下标
@@ -74,3 +99,4 @@ public class Solution {
         return res;
     }
 }
+
