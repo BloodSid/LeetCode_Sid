6e95a11947bcce85480108e3248ea134353bae43
SidXu <892284811@qq.com>
2024-04-15 22:32:33 +0800

    706. 设计哈希映射 链表 拉链法 AC 14ms

--- leetcodeEditor/src/main/java/leetcode/editor/cn/DesignHashmapSolution.java
+++ leetcodeEditor/src/main/java/leetcode/editor/cn/DesignHashmapSolution.java
@@ -1,7 +1,5 @@
 package leetcode.editor.cn;
 
-import java.util.*;
-
 /**
  * 设计哈希映射
  *
@@ -12,23 +10,70 @@ import java.util.*;
 public class DesignHashmapSolution {
 //leetcode submit region begin(Prohibit modification and deletion)
 class MyHashMap {
-    int[] hash;
+
+    private final int m;
+    private final Node[] data;
+    class Node {
+        int key;
+        int val;
+        Node next;
+        Node(int key) {
+            this.key = key;
+        }
+
+        Node(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
 
     public MyHashMap() {
-        hash = new int[1_000_001];
-        Arrays.fill(hash, -1);
+        m = 13337;
+        data = new Node[m];
     }
 
     public void put(int key, int value) {
-        hash[key] = value;
+        Node head = data[key % m];
+        if (head == null) {
+            data[key % m] = new Node(key, value);
+            return;
+        }
+        while (true) {
+            if (head.key == key) {
+                head.val = value;
+                return;
+            }
+            if (head.next == null) {
+                break;
+            }
+            head = head.next;
+        }
+        // 尾插法
+        head.next = new Node(key, value);
     }
 
     public int get(int key) {
-        return hash[key];
+        return getVal(data[key % m], key);
     }
 
     public void remove(int key) {
-        hash[key] = -1;
+        Node pre = new Node(-1);
+        int offset = key % m;
+        pre.next = data[offset];
+        for (Node cur = pre; cur.next != null; cur = cur.next) {
+            if (cur.next.key == key) {
+                cur.next = cur.next.next;
+                break;
+            }
+        }
+        data[offset] = pre.next;
+    }
+
+    private int getVal(Node head, int key) {
+        for (; head != null; head = head.next) {
+            if (head.key == key) return head.val;
+        }
+        return -1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
